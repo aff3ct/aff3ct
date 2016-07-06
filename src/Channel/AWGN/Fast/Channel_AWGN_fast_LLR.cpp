5a296986493244286491ef2e01e27404a6573f26
Adrien Cassagne <adrien.cassagne@inria.fr>
2016-07-06 11:50:02 +0200

    Turn on SIMD code.

--- src/Channel/AWGN/Fast/Channel_AWGN_fast_LLR.cpp
+++ src/Channel/AWGN/Fast/Channel_AWGN_fast_LLR.cpp
@@ -38,58 +38,57 @@ void Channel_AWGN_fast_LLR<R>
 	for (auto i = 0; i < loop_size; i++)
 	{
 		Y_N[i] = this->dist(this->rd_engine);
-		if (Y_N[i] == 0)
-			std::cout << "PROUTTTT" << std::endl;
+		// if (Y_N[i] == 0)
+		// 	std::cout << "PROUTTTT" << std::endl;
 	}
 
-	// const mipp::Reg<R> sf       = this->scaling_factor;
-	// const mipp::Reg<R> sigma    = this->sigma;
-	// const mipp::Reg<R> twopi    = 2.0 * 3.14159265358979323846;
-	// const mipp::Reg<R> one      = 1.0;
-	// const mipp::Reg<R> minustwo = -2.0;
+	const mipp::Reg<R> sf       = this->scaling_factor;
+	const mipp::Reg<R> sigma    = this->sigma;
+	const mipp::Reg<R> twopi    = 2.0 * 3.14159265358979323846;
+	const mipp::Reg<R> one      = 1.0;
+	const mipp::Reg<R> minustwo = -2.0;
 
-	// for (auto i = 0; i < loop_size; i += mipp::nElReg<R>() * 2) 
-	// {
-	// 	// const auto u1 = one - mipp::Reg<R>(&Y_N[i                    ]); // [0, 1) -> (0, 1]
-	// 	const auto u1 = mipp::Reg<R>(&Y_N[i                    ]);
-	// 	const auto u2 = mipp::Reg<R>(&Y_N[i + mipp::nElReg<R>()]);
-
-	// 	const auto radius = mipp::sqrt(minustwo * mipp::log(u1)) * sigma;
-	// 	const auto theta  = twopi * u2;
-
-	// 	mipp::Reg<R> sintheta, costheta;
-	// 	mipp::sincos(theta, sintheta, costheta);
-
-	// 	// fmadd(a, b, c) = a * b + c
-	// 	auto awgn1 = mipp::fmadd(radius, costheta, mipp::Reg<R>(&X_N[i                    ])) * sf;
-	// 	auto awgn2 = mipp::fmadd(radius, sintheta, mipp::Reg<R>(&X_N[i + mipp::nElReg<R>()])) * sf;
-
-	// 	awgn1.store(&Y_N[i                    ]);
-	// 	awgn2.store(&Y_N[i + mipp::nElReg<R>()]);
-	// }
-
-
-	auto twopi = (R)(2.0 * 3.14159265358979323846);
-	for (auto i = 0; i < loop_size; i += 2)
+	for (auto i = 0; i < loop_size; i += mipp::nElReg<R>() * 2) 
 	{
-		// auto x  = (R)sqrt(-2.0 * log((R)rand() / (R)2147483647));
-		// auto y  = (R)rand() / (R)2147483647;
-		const auto u1 = Y_N[i +0];
-		const auto u2 = Y_N[i +1];
+		// const auto u1 = one - mipp::Reg<R>(&Y_N[i                    ]); // [0, 1) -> (0, 1]
+		const auto u1 = mipp::Reg<R>(&Y_N[i                    ]);
+		const auto u2 = mipp::Reg<R>(&Y_N[i + mipp::nElReg<R>()]);
 
-		const auto radius = (R)std::sqrt((R)-2.0 * std::log(u1)) * sigma;
+		const auto radius = mipp::sqrt(minustwo * mipp::log(u1)) * sigma;
 		const auto theta  = twopi * u2;
 
-		const auto sintheta = std::sin(theta);
-		const auto costheta = std::cos(theta);
+		mipp::Reg<R> sintheta, costheta;
+		mipp::sincos(theta, sintheta, costheta);
 
-		Y_N[i +0] = (X_N[i +0] + radius * sintheta) * scaling_factor;
-		Y_N[i +1] = (X_N[i +1] + radius * costheta) * scaling_factor;
+		// fmadd(a, b, c) = a * b + c
+		auto awgn1 = mipp::fmadd(radius, costheta, mipp::Reg<R>(&X_N[i                    ])) * sf;
+		auto awgn2 = mipp::fmadd(radius, sintheta, mipp::Reg<R>(&X_N[i + mipp::nElReg<R>()])) * sf;
+
+		awgn1.store(&Y_N[i                    ]);
+		awgn2.store(&Y_N[i + mipp::nElReg<R>()]);
 	}
 
-	for (auto i = 0; i < loop_size; i++)
-		if (std::isnan(Y_N[i]))
-			std::cout << "NaN" << std::endl;
+	// auto twopi = (R)(2.0 * 3.14159265358979323846);
+	// for (auto i = 0; i < loop_size; i += 2)
+	// {
+	// 	// auto x  = (R)sqrt(-2.0 * log((R)rand() / (R)2147483647));
+	// 	// auto y  = (R)rand() / (R)2147483647;
+	// 	const auto u1 = Y_N[i +0];
+	// 	const auto u2 = Y_N[i +1];
+
+	// 	const auto radius = (R)std::sqrt((R)-2.0 * std::log(u1)) * sigma;
+	// 	const auto theta  = twopi * u2;
+
+	// 	const auto sintheta = std::sin(theta);
+	// 	const auto costheta = std::cos(theta);
+
+	// 	Y_N[i +0] = (X_N[i +0] + radius * sintheta) * scaling_factor;
+	// 	Y_N[i +1] = (X_N[i +1] + radius * costheta) * scaling_factor;
+	// }
+
+	// for (auto i = 0; i < loop_size; i++)
+	// 	if (std::isnan(Y_N[i]))
+	// 		std::cout << "NaN" << std::endl;
 }
 
 // ==================================================================================== explicit template instantiation 
