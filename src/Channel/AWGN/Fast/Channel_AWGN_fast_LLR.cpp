919b203ee99c990db44762cf672cb37522627c71
Adrien Cassagne <adrien.cassagne@inria.fr>
2016-07-08 09:37:20 +0200

    Improve the fast channel by supporting all frame sizes.

--- src/Channel/AWGN/Fast/Channel_AWGN_fast_LLR.cpp
+++ src/Channel/AWGN/Fast/Channel_AWGN_fast_LLR.cpp
@@ -61,56 +61,70 @@ template <typename R>
 void Channel_AWGN_fast_LLR<R>
 ::add_noise(const mipp::vector<R>& X_N, mipp::vector<R>& Y_N)
 {
-	assert(X_N.size() % 2                       == 0         );
-	assert(X_N.size()                           == Y_N.size());
-	assert(scaling_factor                       != 0         );
-	assert(sigma                                != 0         );
-	assert(X_N.size() % (2 * mipp::nElReg<R>()) == 0         );
+	assert(X_N.size()     == Y_N.size());
+	assert(scaling_factor != 0         );
+	assert(sigma          != 0         );
 
-	const mipp::Reg<R> sf       = this->scaling_factor;
-	const mipp::Reg<R> sigma    = this->sigma;
-	const mipp::Reg<R> twopi    = 2.0 * 3.14159265358979323846;
-	const mipp::Reg<R> one      = 1.0;
-	const mipp::Reg<R> minustwo = -2.0;
+	const auto twopi = (R)(2.0 * 3.14159265358979323846);
 
-	// box muller method in the polar form
+	// SIMD version of the Box Muller method in the polar form
+	const mipp::Reg<R> r_sf       = scaling_factor;
+	const mipp::Reg<R> r_sigma    = sigma;
+	const mipp::Reg<R> r_twopi    = twopi;
+	const mipp::Reg<R> r_minustwo = -2.0;
+
 	const auto loop_size = (int)Y_N.size();
-	for (auto i = 0; i < loop_size; i += mipp::nElReg<R>() * 2) 
+	const auto vec_loop_size = (int)((loop_size / mipp::nElReg<R>()) * mipp::nElReg<R>());
+	for (auto i = 0; i < vec_loop_size; i += mipp::nElReg<R>() * 2) 
 	{
-		const auto u1 = get_random_fast();
-		const auto u2 = get_random_fast();
+		const auto r_u1 = get_random_fast();
+		const auto r_u2 = get_random_fast();
 
-		const auto radius = mipp::sqrt(minustwo * mipp::log(u1)) * sigma;
-		const auto theta  = twopi * u2;
+		const auto r_radius = mipp::sqrt(r_minustwo * mipp::log(r_u1)) * r_sigma;
+		const auto r_theta  = r_twopi * r_u2;
 
-		mipp::Reg<R> sintheta, costheta;
-		mipp::sincos(theta, sintheta, costheta);
+		mipp::Reg<R> r_sintheta, r_costheta;
+		mipp::sincos(r_theta, r_sintheta, r_costheta);
 
 		// fmadd(a, b, c) = a * b + c
-		auto awgn1 = mipp::fmadd(radius, costheta, mipp::Reg<R>(&X_N[i                    ])) * sf;
-		auto awgn2 = mipp::fmadd(radius, sintheta, mipp::Reg<R>(&X_N[i + mipp::nElReg<R>()])) * sf;
+		auto r_awgn1 = mipp::fmadd(r_radius, r_costheta, mipp::Reg<R>(&X_N[i                    ])) * r_sf;
+		auto r_awgn2 = mipp::fmadd(r_radius, r_sintheta, mipp::Reg<R>(&X_N[i + mipp::nElReg<R>()])) * r_sf;
 
-		awgn1.store(&Y_N[i                    ]);
-		awgn2.store(&Y_N[i + mipp::nElReg<R>()]);
+		r_awgn1.store(&Y_N[i                    ]);
+		r_awgn2.store(&Y_N[i + mipp::nElReg<R>()]);
 	}
 
-	// // seq version (equivalent to the AWGN_std channel)
-	// const auto loop_size = (int)Y_N.size();
-	// auto twopi = (R)(2.0 * 3.14159265358979323846);
-	// for (auto i = 0; i < loop_size; i += 2)
-	// {
-	// 	const auto u1 = get_random();
-	// 	const auto u2 = get_random();
+	// seq version of the Box Muller method in the polar form
+	const auto seq_loop_size = (int)(loop_size / 2) * 2;
+	for (auto i = vec_loop_size; i < seq_loop_size; i += 2)
+	{
+		const auto u1 = get_random();
+		const auto u2 = get_random();
 
-	// 	const auto radius = (R)std::sqrt((R)-2.0 * std::log(u1)) * sigma;
-	// 	const auto theta  = twopi * u2;
+		const auto radius = (R)std::sqrt((R)-2.0 * std::log(u1)) * sigma;
+		const auto theta  = twopi * u2;
 
-	// 	const auto sintheta = std::sin(theta);
-	// 	const auto costheta = std::cos(theta);
+		const auto sintheta = std::sin(theta);
+		const auto costheta = std::cos(theta);
 
-	// 	Y_N[i +0] = (X_N[i +0] + radius * sintheta) * scaling_factor;
-	// 	Y_N[i +1] = (X_N[i +1] + radius * costheta) * scaling_factor;
-	// }
+		Y_N[i +0] = (radius * sintheta + X_N[i +0]) * scaling_factor;
+		Y_N[i +1] = (radius * costheta + X_N[i +1]) * scaling_factor;
+	}
+
+	// distribute the last odd element
+	if (loop_size != seq_loop_size)
+	{
+		const auto u1 = get_random();
+		const auto u2 = get_random();
+
+		const auto radius = (R)std::sqrt((R)-2.0 * std::log(u1)) * sigma;
+		const auto theta  = twopi * u2;
+
+		const auto sintheta = std::sin(theta);
+
+		Y_N[loop_size -1] = (radius * sintheta + X_N[loop_size -1]) * scaling_factor;
+	}	
+
 }
 
 // ==================================================================================== explicit template instantiation 
