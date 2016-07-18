1d82182fc8d7c73c0175f045f07726568085d955
Adrien Cassagne <adrien.cassagne@inria.fr>
2016-07-18 17:13:18 +0200

    Fix bad perf for the 2-PSK + cosmetics.

--- src/Modulator/PSK/Modulator_PSK.cpp
+++ src/Modulator/PSK/Modulator_PSK.cpp
@@ -12,30 +12,27 @@
 template <typename B, typename R>
 Modulator_PSK<B,R>
 ::Modulator_PSK(const unsigned int bits_per_symbol, const R sigma)
-: bits_per_symbol(bits_per_symbol),
-  nbr_symbols(1<<bits_per_symbol),
-  sigma(sigma),
-  sqrtEs(sqrt(2.0*(this->nbr_symbols-1)/3.0))
+: bits_per_symbol(bits_per_symbol     ),
+  nbr_symbols    (1 << bits_per_symbol),
+  sigma          (sigma               ),
+  constellation  (nbr_symbols         )
 {
 	mipp::vector<B> bits(this->bits_per_symbol);
-	this->Constellation.resize(this->nbr_symbols);
 
-	for (unsigned j = 0; j < this->nbr_symbols; j++)
+	for (auto j = 0; j < this->nbr_symbols; j++)
 	{
-		for (unsigned l=0; l< this->bits_per_symbol; l++)
-		{
+		for (auto l = 0; l< this->bits_per_symbol; l++)
 			bits[l] = (j >> l) & 1;
-		}
 
-		this->Constellation[j] = this->bits_to_symbol(&bits[0]);
+		this->constellation[j] = this->bits_to_symbol(&bits[0]);
 	}
 }
 
 template <typename B, typename R>
 Modulator_PSK<B,R>
-::~Modulator_PSK() {
+::~Modulator_PSK()
+{
 }
-
 
 /*
  * int get_buffer_size(const int N)
@@ -44,67 +41,65 @@ Modulator_PSK<B,R>
  */
 template <typename B, typename R>
 int Modulator_PSK<B,R>
-:: get_buffer_size(const int N)
+::get_buffer_size(const int N)
 {
-	//assert(N % this->bits_per_symbol == 0);
-	return std::ceil((float)N / (float)this->bits_per_symbol)*2;
+	return std::ceil((float)N / (float)this->bits_per_symbol) * 2;
 }
-
 
 /*
  * Mapping function
  */
 template <typename B, typename R>
 std::complex<R> Modulator_PSK<B,R>
-::bits_to_symbol (const B* bits) const
- {
+::bits_to_symbol(const B* bits) const
+{
 	auto bps = this->bits_per_symbol;
-	R symbol;
 
-	symbol = 1- ((R)bits[0] + (R)bits[0]);
-	for (unsigned j=1; j<bps; j++)
-	{
-		symbol =  (1.0 - ((R)bits[j] + (R)bits[j])) * ((1<<j) - symbol);
-	}
-	return std::complex<R>(std::cos((symbol+1)*M_PI/this->nbr_symbols),std::sin((symbol+1)*M_PI/this->nbr_symbols));
+	R symbol = 1 - ((R)bits[0] + (R)bits[0]);
+	for (auto j = 1; j < bps; j++)
+		symbol = (1.0 - ((R)bits[j] + (R)bits[j])) * ((1 << j) - symbol);
 
- }
+	return std::complex<R>(std::cos((symbol +1) * M_PI / this->nbr_symbols), 
+	                       std::sin((symbol +1) * M_PI / this->nbr_symbols));
+}
 
 /*
  * Modulator
  */
 template <typename B,typename R>
 void Modulator_PSK<B,R>
-:: modulate(const mipp::vector<B>& X_N1, mipp::vector<R>& X_N2) const
+::modulate(const mipp::vector<B>& X_N1, mipp::vector<R>& X_N2) const
 {
-	auto size_out    = X_N2.size();
-	auto size_in     = X_N1.size();
-	auto bps         = this->bits_per_symbol;
-	unsigned int idx = 0;
-	auto size_rest   = size_in % bps;
-	//std::complex<R> symbol;
-	//std::cout << "Size In : " << size_in << std::endl;
-	//std::cout << "Size Out : " << size_out << std::endl;
-	for (unsigned i = 0; i < size_out/2-1; i++)
+	auto size_in  = (int)X_N1.size();
+	auto size_out = (int)X_N2.size();
+	auto bps      = this->bits_per_symbol;
+
+	auto main_loop_size = size_in / bps;
+	for (auto i = 0; i < main_loop_size; i++)
 	{
-		//symbol = bits_to_symbol(&X_N1[i*bps]);
-		idx = 0;
-		for (unsigned j = 0; j < bps; j++)
-		{
-			idx += (1 << j) * X_N1[i*bps + j];
-		}
-		 X_N2[2*i] = this->Constellation[idx].real();
-		 X_N2[2*i+1] = this->Constellation[idx].imag();
+		// compute the symbol "on the fly"
+		// auto symbol = bits_to_symbol(&X_N1[i*bps]);
+
+		// determine the symbol with a lookup table
+		unsigned idx = 0;
+		for (auto j = 0; j < bps; j++)
+			idx += (1 << j) * X_N1[i * bps +j];
+		auto symbol = this->constellation[idx];
+
+		 X_N2[2*i +0] = symbol.real();
+		 X_N2[2*i +1] = symbol.imag();
 	}
 
-	idx = 0;
-	for (unsigned j = 0; j < size_rest ;j++)
+	// last elements if "size_in" is not a multiple of the number of bits per symbol
+	if (main_loop_size * bps < size_in)
 	{
-		//std::cout<< "j= "<< j << std::endl;
-		idx += (1 << j) * X_N1[((size_out/2-1)*bps + j)];
+		unsigned idx = 0;
+		for (auto j = 0; j < size_in - (main_loop_size * bps); j++)
+			idx += (1 << j) * X_N1[main_loop_size * bps +j];
+
+		X_N2[size_out -2] = this->constellation[idx].real();
+		X_N2[size_out -1] = this->constellation[idx].imag();
 	}
-	X_N2[size_out - 2] = this->Constellation[idx].real();
-	X_N2[size_out - 1] = this->Constellation[idx].imag();
 }
 
 /*
@@ -112,37 +107,30 @@ void Modulator_PSK<B,R>
  */
 template <typename B,typename R>
 void Modulator_PSK<B,R>
-:: demodulate(const mipp::vector<R>& Y_N1, mipp::vector<R>& Y_N2) const
+::demodulate(const mipp::vector<R>& Y_N1, mipp::vector<R>& Y_N2) const
 {
-	auto size = Y_N2.size();
+	auto size = (int)Y_N2.size();
 	std::complex<R> complex_Yk;
-	R L0;
-	R L1;
-	unsigned k, b;
 
-	R sigma2 = this->sigma*this->sigma;
-
-	for (unsigned n = 0; n < size; n++)// Boucle sur les LLRs
+	R sigma2 = this->sigma * this->sigma;
+	for (auto n = 0; n < size; n++)// boucle sur les LLRs
 	{
-		L0 = -std::numeric_limits<R>::infinity();
-		L1 = -std::numeric_limits<R>::infinity();
-		b = n % this->bits_per_symbol; // position du bit
-		k = n / this->bits_per_symbol; // Position du symbole
+		auto L0 = -std::numeric_limits<R>::infinity();
+		auto L1 = -std::numeric_limits<R>::infinity();
+		auto b  = n % this->bits_per_symbol; // position du bit
+		auto k  = n / this->bits_per_symbol; // position du symbole
 
-		complex_Yk =std::complex<R>(Y_N1[2*k],Y_N1[2*k+1]);
+		complex_Yk = std::complex<R>(Y_N1[2*k], Y_N1[2*k+1]);
 
-		for (unsigned j = 0; j < this->nbr_symbols; j++)
-		{
+		for (auto j = 0; j < this->nbr_symbols; j++)
 			if ( (j & (1 << b)) == 0 )
-				L0 = max_star(L0,-std::norm(complex_Yk-this->Constellation[j])/(sigma2));
+				L0 = max_star(L0, -std::norm(complex_Yk - this->constellation[j]) / sigma2);
 			else
-				L1 = max_star(L1,-std::norm(complex_Yk-this->Constellation[j])/(sigma2));
-		}
-		Y_N2[n] = L0-L1;
+				L1 = max_star(L1, -std::norm(complex_Yk - this->constellation[j]) / sigma2);
+
+		Y_N2[n] = L0 - L1;
 	}
 }
-
-
 
 // ==================================================================================== explicit template instantiation 
 #include "../../Tools/types.h"
--- src/Modulator/PSK/Modulator_PSK.hpp
+++ src/Modulator/PSK/Modulator_PSK.hpp
@@ -10,22 +10,23 @@
 template <typename B, typename R>
 class Modulator_PSK : public Modulator<B,R>
 {
-public:
+private:
+	const int bits_per_symbol;
+	const int nbr_symbols;
+	const R sigma;
+	mipp::vector<std::complex<R>> constellation;
 
-	Modulator_PSK(const unsigned int bits_per_symbol = 2,const R sigma = 1.0);
+public:
+	Modulator_PSK(const unsigned int bits_per_symbol = 2, const R sigma = 1.0);
     virtual ~Modulator_PSK();
 
-	virtual void modulate(const mipp::vector<B>& X_N1, mipp::vector<R>& X_N2) const;
+	virtual void   modulate(const mipp::vector<B>& X_N1, mipp::vector<R>& X_N2) const;
 	virtual void demodulate(const mipp::vector<R>& Y_N1, mipp::vector<R>& Y_N2) const;
+	
 	int get_buffer_size(const int N);
 
-	int bits_per_symbol;
-	int nbr_symbols;
-	R sigma;
-	R sqrtEs;
-	mipp::vector<std::complex<R>> Constellation;
 private:
-	std::complex<R> bits_to_symbol (const B* bits) const;
+	inline std::complex<R> bits_to_symbol(const B* bits) const;
 };
 
 #endif // MODULATOR_PSK_HPP_
