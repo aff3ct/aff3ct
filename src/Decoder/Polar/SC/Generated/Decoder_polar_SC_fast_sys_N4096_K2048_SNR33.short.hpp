4c4d0533eea31f5e67cb989226d1dc9556aeb62a
Adrien Cassagne <adrien.cassagne@inria.fr>
2016-08-29 12:52:05 +0200

    Fix a bug in the N4096 SC generated decoder.

--- src/Decoder/Polar/SC/Generated/Decoder_polar_SC_fast_sys_N4096_K2048_SNR33.short.hpp
+++ src/Decoder/Polar/SC/Generated/Decoder_polar_SC_fast_sys_N4096_K2048_SNR33.short.hpp
@@ -139,8 +139,8 @@ template <typename B, typename R, class API_polar>
 class Decoder_polar_SC_fast_sys_N4096_K2048_SNR33 : public Decoder_polar_SC_fast_sys<B, R, API_polar>
 {
 public:
-	Decoder_polar_SC_fast_sys_N4096_K2048_SNR33(const int& N, const mipp::vector<B>& frozen_bits)
-	: Decoder_polar_SC_fast_sys<B, R, API_polar>(N, frozen_bits)
+	Decoder_polar_SC_fast_sys_N4096_K2048_SNR33(const int& K, const int& N, const mipp::vector<B>& frozen_bits)
+	: Decoder_polar_SC_fast_sys<B, R, API_polar>(K, N, frozen_bits)
 	{
 		assert(N == 4096);
 		
