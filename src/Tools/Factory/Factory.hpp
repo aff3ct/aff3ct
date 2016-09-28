3712bcab56b25b11915a5116e1f4768969f09b1d
Adrien Cassagne <adrien.cassagne@inria.fr>
2016-09-28 15:13:38 +0200

    Working on the doc.

--- src/Tools/Factory/Factory.hpp
+++ src/Tools/Factory/Factory.hpp
@@ -1,6 +1,21 @@
+/*!
+ * \file
+ * \brief Class builder.
+ *
+ * \section LICENSE
+ * This file is under MIT license (https://opensource.org/licenses/MIT).
+ */
 #ifndef FACTORY_HPP
 #define FACTORY_HPP
 
+/*!
+ * \class Factory
+ *
+ * \brief Class builder.
+ *
+ * A factory is a structure dedicated to the object creation. The common usage is to call the build method which returns
+ * a new allocated object.
+ */
 struct Factory {};
 
 #endif /* FACTORY_HPP */
