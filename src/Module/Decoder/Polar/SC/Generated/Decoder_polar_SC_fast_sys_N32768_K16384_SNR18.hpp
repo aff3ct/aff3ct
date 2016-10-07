4676b56ecf247b5ad67464162bc2744b91226092
Adrien Cassagne <adrien.cassagne@inria.fr>
2016-10-07 11:04:04 +0200

    Fix a compilation error in some generated decoders.

--- src/Module/Decoder/Polar/SC/Generated/Decoder_polar_SC_fast_sys_N32768_K16384_SNR18.hpp
+++ src/Module/Decoder/Polar/SC/Generated/Decoder_polar_SC_fast_sys_N32768_K16384_SNR18.hpp
@@ -1035,8 +1035,8 @@ template <typename B, typename R, class API_polar>
 class Decoder_polar_SC_fast_sys_N32768_K16384_SNR18 : public Decoder_polar_SC_fast_sys<B, R, API_polar>
 {
 public:
-	Decoder_polar_SC_fast_sys_N32768_K16384_SNR18(const int& N, const mipp::vector<B>& frozen_bits)
-	: Decoder_polar_SC_fast_sys<B, R, API_polar>(N, frozen_bits)
+	Decoder_polar_SC_fast_sys_N32768_K16384_SNR18(const int& K, const int& N, const mipp::vector<B>& frozen_bits)
+	: Decoder_polar_SC_fast_sys<B, R, API_polar>(K, N, frozen_bits)
 	{
 		assert(N == 32768);
 		
