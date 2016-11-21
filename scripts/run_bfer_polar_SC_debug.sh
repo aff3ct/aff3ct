25441009d0f2eeb83f21ba05393b735d6312b954
Adrien Cassagne <adrien.cassagne@inria.fr>
2016-11-21 15:07:30 +0100

    Fix debug script.

--- scripts/run_bfer_polar_SC_debug.sh
+++ scripts/run_bfer_polar_SC_debug.sh
@@ -11,6 +11,7 @@ cd ../build
     --sim-snr-max     0.50       \
     --sim-snr-step    0.1        \
     --mnt-max-fe      1          \
+    --sim-threads     1          \
     --sim-debug                  \
     --dec-type        SC         \
     --dec-implem      NAIVE      \
