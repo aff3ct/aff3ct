b55fe1835db3bdd6affd6af84beb80d6acabe8e1
Adrien Cassagne <adrien.cassagne@inria.fr>
2017-07-22 11:54:43 +0200

    Update the scripts.

--- scripts/run_bfer_polar_SC_small.sh
+++ scripts/run_bfer_polar_SC_small.sh
@@ -2,15 +2,15 @@
 
 cd ../build
 
-./bin/aff3ct                     \
-    --sim-type        BFER       \
-    --cde-type        POLAR      \
-    -K                128        \
-    -N                256        \
-    --sim-snr-min     0.50       \
-    --sim-snr-max     4.01       \
-    --sim-snr-step    0.1        \
-    --mnt-max-fe      100        \
-    --dec-type        SC         \
-    --dec-implem      NAIVE      \
+./bin/aff3ct             \
+    --sim-type     BFER  \
+    --sim-cde-type POLAR \
+    -K             128   \
+    -N             256   \
+    --sim-snr-min  0.50  \
+    --sim-snr-max  4.01  \
+    --sim-snr-step 0.1   \
+    --mnt-max-fe   100   \
+    --dec-type     SC    \
+    --dec-implem   NAIVE \
     $*
--- scripts/run_exit_polar_SCAN.sh
+++ scripts/run_exit_polar_SCAN.sh
@@ -2,18 +2,18 @@
 
 cd ../build
 
-./bin/aff3ct                     \
-    --sim-type       EXIT        \
-    --sim-cde-type   POLAR       \
-    -K               1194        \
-    -N               2048        \
-    --sim-snr-min    -3          \
-    --sim-snr-max    -3          \
-    --sim-siga-min   0.0         \
-    --sim-siga-max   5.01        \
-    --sim-snr-step   0.1         \
-    --chn-type       AWGN        \
-    --dec-type       SCAN        \
-    --dec-implem     NAIVE       \
-    --enc-fb-sigma   0.8         \
+./bin/aff3ct             \
+    --sim-type     EXIT  \
+    --sim-cde-type POLAR \
+    -K             1194  \
+    -N             2048  \
+    --sim-snr-min  -3    \
+    --sim-snr-max  -3    \
+    --sim-siga-min 0.0   \
+    --sim-siga-max 5.01  \
+    --sim-snr-step 0.1   \
+    --chn-type     AWGN  \
+    --dec-type     SCAN  \
+    --dec-implem   NAIVE \
+    --enc-fb-sigma 0.8   \
     $*
