2768bbe0e50bc1460d670ea0d4f6d3d6399ddd41
Olivier HARTMANN <olivier.hartmann@u-bordeaux.fr>
2017-12-21 14:28:19 +0100

    Add Boolean argument type; Make turbo pattern argument works with a 2D list argument type

---
+++ src/Tools/Arguments/Argument_type/Boolean.hpp
@@ -0,0 +1,59 @@
+#ifndef ARGUMENT_TYPE_BOOLEAN_HPP_
+#define ARGUMENT_TYPE_BOOLEAN_HPP_
+
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
+#include "Argument_type.hpp"
+
+namespace aff3ct
+{
+namespace tools
+{
+
+template <typename T = bool>
+class Boolean : public Argument_type_limited<T>
+{
+public:
+	Boolean()
+	: Argument_type_limited<T>("boolean", {new Including_set<bool>({(bool)0, (bool)1})})
+	{ }
+
+	virtual ~Boolean() {};
+
+	virtual Boolean<T>* clone() const
+	{
+		auto clone = new Boolean<T>(*this);
+
+		return dynamic_cast<Boolean<T>*>(this->clone_ranges(clone));
+	}
+
+	virtual int convert(const std::string& val) const
+	{
+		return std::stoi(val);
+	}
+
+	virtual void check(const std::string& val) const
+	{
+		int bool_num;
+
+		try
+		{
+			bool_num = this->convert(val);
+		}
+		catch(std::exception&)
+		{
+			throw std::runtime_error("shall be a boolean");
+		}
+
+		if (bool_num != 1 && bool_num != 0)
+			throw std::runtime_error("shall be a boolean");
+
+		this->check_ranges((bool)bool_num);
+	}
+};
+
+}
+}
+#endif /* ARGUMENT_TYPE_BOOLEAN_HPP_ */
