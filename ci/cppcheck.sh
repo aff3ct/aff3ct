f3852c1cd67d2d50e7f779372ee8b7299b88bfea
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-01-10 16:03:49 +0100

    Fix prev. commit.

--- ci/cppcheck.sh
+++ ci/cppcheck.sh
@@ -2,7 +2,7 @@
 
 find .\/src\/ -type f -follow -print | grep "[.]h$\|[.]hpp$\|[.]hxx$\|[.]cpp$" > src_files.txt
 cppcheck --force --enable=all --std=c++11 --file-list=src_files.txt 2> cppcheck.log
-#cat cppcheck.log | grep "(error)" > cppcheck_error.log
+cat cppcheck.log | grep "(error)" > cppcheck_error.log
 cat cppcheck.log | grep "(warning)" > cppcheck_warning.log
 cat cppcheck.log | grep "(performance)" > cppcheck_performance.log
 cat cppcheck.log | grep "(style)" > cppcheck_style.log
