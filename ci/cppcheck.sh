5d81e5d8e617f593ad3b5c1e5cd62e9f97cadd01
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-01-10 15:47:28 +0100

    Add a static analysis with cppcheck in the CI.

---
+++ ci/cppcheck.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+find .\/src\/ -type f -follow -print | grep "[.]h$\|[.]hpp$\|[.]hxx$\|[.]cpp$" > src_files.txt
+cppcheck --force --enable=all --std=c++11 --file-list=src_files.txt 2> cppcheck.log
+cat cppcheck.log | grep "(error)" > cppcheck_error.log
+cat cppcheck.log | grep "(warning)" > cppcheck_warning.log
+cat cppcheck.log | grep "(performance)" > cppcheck_performance.log
+cat cppcheck.log | grep "(style)" > cppcheck_style.log
+cat cppcheck.log | grep "(portability)" > cppcheck_portability.log
+cat cppcheck.log | grep "(information)" > cppcheck_information.log
+cat cppcheck.log | grep "(unusedFunction)" > cppcheck_unusedFunction.log
+cat cppcheck.log | grep "(missingInclude)" > cppcheck_missingInclude.log
+
+COUNT=$(wc -l < cppcheck_error.log )
+
+if [ $COUNT -gt 0 ]; then
+  echo "Error count is $COUNT! cppcheck run failed :-(.";
+  exit 1;
+else
+  echo "Error count is $COUNT... Ok :-)."
+fi
