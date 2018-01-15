8a77379d4201fa453a1a2fd94782d1d38f93198e
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-01-15 16:53:31 +0100

    Allow the cppcheck script to fail.

--- ci/cppcheck.sh
+++ ci/cppcheck.sh
@@ -3,7 +3,7 @@ set -x
 
 #cppcheck --suppress=missingIncludeSystem -I./src/ --force --enable=all --std=c++11 -U_MSC_VER ./src/ 2> cppcheck_all.log
 find .\/src\/ -type f -follow -print | grep "[.]h$\|[.]hpp$\|[.]hxx$\|[.]cpp$" > src_files.txt
-cppcheck --suppress=missingIncludeSystem --force --enable=all --std=c++11 -U_MSC_VER --file-list=src_files.txt 2> cppcheck_all.log
+cppcheck --language=c++ --suppress=missingIncludeSystem --force --enable=all --std=c++11 -U_MSC_VER --file-list=src_files.txt 2> cppcheck_all.log
 cat cppcheck_all.log | grep "(error)"          > cppcheck_error.log
 cat cppcheck_all.log | grep "(warning)"        > cppcheck_warning.log
 cat cppcheck_all.log | grep "(performance)"    > cppcheck_performance.log
@@ -13,17 +13,17 @@ cat cppcheck_all.log | grep "(information)"    > cppcheck_information.log
 cat cppcheck_all.log | grep "(unusedFunction)" > cppcheck_unusedFunction.log
 cat cppcheck_all.log | grep "(missingInclude)" > cppcheck_missingInclude.log
 
-## Uncomment this block if you want a failure when there is errors
-#COUNT=$(wc -l < cppcheck_error.log )
-#
-#if [ $COUNT -gt 0 ]; then
-#  echo "Error count is $COUNT! cppcheck run failed :-(.";
-#  echo ""
-#  echo "Errors list:"
-#  cat cppcheck_error.log
-#  exit 1;
-#else
-#  echo "There is no error :-)."
-#fi
+# Uncomment this block if you want a failure when there is errors
+COUNT=$(wc -l < cppcheck_error.log )
+
+if [ $COUNT -gt 5 ]; then
+	echo "Error count is $COUNT! cppcheck run failed :-(.";
+	echo ""
+	echo "Errors list:"
+	cat cppcheck_error.log
+	exit 1;
+else
+	echo "There is no error :-)."
+fi
 
 exit 0;
