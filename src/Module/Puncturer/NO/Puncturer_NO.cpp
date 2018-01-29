59779eb6c7e4c2cd2e2097900cceddf08aab52a4
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-01-29 10:02:41 +0100

    Fix the fix...

--- src/Module/Puncturer/NO/Puncturer_NO.cpp
+++ src/Module/Puncturer/NO/Puncturer_NO.cpp
@@ -28,7 +28,7 @@ void Puncturer_NO<B,Q>
 
 template <typename B, typename Q>
 void Puncturer_NO<B,Q>
-::_depuncture(const Q *Y_N1, Q *Y_N2, const int frame_id) const;
+::_depuncture(const Q *Y_N1, Q *Y_N2, const int frame_id) const
 {
 	std::copy(Y_N1, Y_N1 + this->N, Y_N2);
 }
