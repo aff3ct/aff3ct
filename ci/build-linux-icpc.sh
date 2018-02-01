86454e7267f71b74ff47c6f11bc1b5a604402c95
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-02-01 14:31:00 +0100

    Add a build test for the Intel compiler.

---
+++ ci/build-linux-icpc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+
+source /opt/intel/vars-intel.sh
+mkdir build
+cd build
+#cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=icpc -DCMAKE_BUILD_TYPE=Release -DENABLE_GSL=ON -DENABLE_MKL=ON -DCMAKE_CXX_FLAGS="-funroll-loops -march=native -DMULTI_PREC -std=c++11 -Wall -Warray-bounds -Wchar-subscripts -Wcomment -Wenum-compare -Wformat -Wuninitialized -Wmaybe-uninitialized -Wmain -Wnarrowing -Wnonnull -Wparentheses -Wpointer-sign -Wreorder -Wreturn-type -Wsign-compare -Wsequence-point -Wtrigraphs -Wunused-function -Wunused-but-set-variable -Wunused-variable -Wwrite-strings"
+cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=icpc -DCMAKE_BUILD_TYPE=Release -DENABLE_GSL=ON -DENABLE_MKL=ON -DCMAKE_CXX_FLAGS="-funroll-loops -march=native -DMULTI_PREC -std=c++11 -Wall"
+THREADS=$(grep -c ^processor /proc/cpuinfo)
+make -j $THREADS
+
+./bin/aff3ct --version
+./bin/aff3ct -C POLAR -m 1.0 -M 3.5 -s 0.25 -K 1755 -N 2048 --chn-implem MKL --crc-poly 32-GZIP --dec-type ASCL --ter-freq 0
