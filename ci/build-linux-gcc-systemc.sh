c241a9070a31a4542261e6b08854d0f0e9ac33db
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-02-15 10:01:50 +0100

    Fix the SystemC build test script.

--- ci/build-linux-gcc-systemc.sh
+++ ci/build-linux-gcc-systemc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -x
 
+cp $SYSTEMC_HOME/FindSystemC.cmake cmake/Modules/
+cp $SYSTEMC_HOME/FindTLM.cmake cmake/Modules/
+
 mkdir build
 cd build
 cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DENABLE_SYSTEMC=ON -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -march=native -DMULTI_PREC"
