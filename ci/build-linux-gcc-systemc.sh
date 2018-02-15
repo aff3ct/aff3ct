c431455dfff0a008567ebc57d054fb2671f7a487
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-02-15 10:37:51 +0100

    Do not show deprecated warnings.

--- ci/build-linux-gcc-systemc.sh
+++ ci/build-linux-gcc-systemc.sh
@@ -6,7 +6,7 @@ cp $SYSTEMC_HOME/FindTLM.cmake cmake/Modules/
 
 mkdir build
 cd build
-cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DENABLE_SYSTEMC=ON -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -march=native -DMULTI_PREC"
+cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DENABLE_SYSTEMC=ON -DCMAKE_CXX_FLAGS="-Wall -Wno-deprecated-declarations -funroll-loops -march=native -DMULTI_PREC"
 THREADS=$(grep -c ^processor /proc/cpuinfo)
 make -j $THREADS
 
