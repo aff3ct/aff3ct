7fd59224fe854303c11df5ad706d9fc9c90e72e9
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-02-15 09:21:36 +0100

    A new compilation tests (8-bit, MPI and SystemC) in the CI.

---
+++ ci/build-linux-gcc-systemc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+
+mkdir build
+cd build
+cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release -DENABLE_SYSTEMC=ON -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -march=native -DMULTI_PREC"
+THREADS=$(grep -c ^processor /proc/cpuinfo)
+make -j $THREADS
+
+./bin/aff3ct --version
+./bin/aff3ct -t 1 -C POLAR -m 1.0 -M 3.5 -s 0.25 -K 1755 -N 2048 --crc-poly 32-GZIP --dec-type ASCL --ter-freq 0
