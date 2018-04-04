bfb32b488e67797f62e3b723c47717073183c987
Olivier HARTMANN <olivier.hartmann@u-bordeaux.fr>
2018-04-04 16:40:51 +0200

    Renaming

--- src/Tools/Math/interpolation.h
+++ src/Tools/Math/interpolation.h
@@ -12,30 +12,30 @@ namespace tools
 {
 
 /*
- * Compute the linear interpolation of xVal from the original data y_data matching with its abscissa x_data.
- * x_data, of length lData, must be sorted and strictly monotonic increasing
+ * Compute the linear interpolation of x_val from the original data y_data matching with its abscissa x_data.
+ * x_data, of length l_data, must be sorted and strictly monotonic increasing
  * x_data and y_data must have the same length
- * If xVal goes out of x_data range, then return the left or right limit value in function of the violated one.
+ * If x_val goes out of x_data range, then return the left or right limit value in function of the violated one.
  */
 template <typename T>
-T linear_interpolation(const T* x_data, const T* y_data, const unsigned lData, const T xVal);
+T linear_interpolation(const T* x_data, const T* y_data, const unsigned l_data, const T x_val);
 
 /*
- * Compute the linear interpolation of xVal from the original data pair (x,y) of length lData.
+ * Compute the linear interpolation of x_val from the original data pair (x,y) of length l_data.
  * data must be sorted and strictly monotonic increasing according to the first element of its pair, x.
- * If xVal goes out of x_data range, then return the left or right limit value in function of the violated one.
+ * If x_val goes out of x_data range, then return the left or right limit value in function of the violated one.
  */
 template <typename T>
-T linear_interpolation(const Point<T>* data, const unsigned lData, const T xVal);
+T linear_interpolation(const Point<T>* data, const unsigned l_data, const T x_val);
 
 /*
- * Compute the linear interpolation of x_vals array, of length lVals, from the original data y_data matching with its abscissa x_data.
- * x_data, of length lData, must be sorted and strictly monotonic increasing
+ * Compute the linear interpolation of x_vals array, of length l_vals, from the original data y_data matching with its abscissa x_data.
+ * x_data, of length l_data, must be sorted and strictly monotonic increasing
  * Computed interpolation is returned in the filled array y_vals of same size than x_vals
  */
 template <typename T>
-void linear_interpolation(const T* x_data, const T* y_data, const unsigned lData,
-                          const T* x_vals,       T* y_vals, const unsigned lVals);
+void linear_interpolation(const T* x_data, const T* y_data, const unsigned l_data,
+                          const T* x_vals,       T* y_vals, const unsigned l_vals);
 
 /*
  * Compute the linear interpolation of x_vals vector, from the original data y_data matching with its abscissa x_data.
--- src/Tools/Math/interpolation.hxx
+++ src/Tools/Math/interpolation.hxx
@@ -13,51 +13,51 @@ namespace tools
 {
 
 template <typename T>
-T linear_interpolation(const T* x_data, const T* y_data, const unsigned lData, const T xVal)
+T linear_interpolation(const T* x_data, const T* y_data, const unsigned l_data, const T x_val)
 {
-	auto x_above = std::lower_bound(x_data, x_data + lData, xVal); // find the position of the first x that is above the xVal
+	auto x_above = std::lower_bound(x_data, x_data + l_data, x_val); // find the position of the first x that is above the x_val
 
 	if (x_above == x_data)
 		return *y_data;
 
-	auto x_below = x_above - 1; // get the position of value just below or equal to xVal
+	auto x_below = x_above - 1; // get the position of value just below or equal to x_val
 	auto y_below = y_data + (x_below - x_data); // get the position of the matching value y of x_below
 
-	if ((x_above == (x_data + lData)) || comp_equal(xVal, *x_below)) // if last or x_below == xVal
+	if ((x_above == (x_data + l_data)) || comp_equal(x_val, *x_below)) // if last or x_below == x_val
 		return *y_below; // same x so take y directly
 
 
 	// compute the interpolation	 y = y0 + (y1-y0)*(x-x0)/(x1-x0);
 	auto y_above = y_below + 1; // get the position of value just above
 
-	return *y_below + (*y_above - *y_below) * (xVal - *x_below) / (*x_above - *x_below);
+	return *y_below + (*y_above - *y_below) * (x_val - *x_below) / (*x_above - *x_below);
 }
 
 template <typename T>
-T linear_interpolation(const Point<T>* data, const unsigned lData, const T xVal)
+T linear_interpolation(const Point<T>* data, const unsigned l_data, const T x_val)
 {
-	// find the position of the first x that is above the xVal
-	auto p_above = std::lower_bound(data, data + lData, xVal, [](const Point<T>& p, const T& x){return p.x() < x;});
+	// find the position of the first x that is above the x_val
+	auto p_above = std::lower_bound(data, data + l_data, x_val, [](const Point<T>& p, const T& x){return p.x() < x;});
 
 	if (p_above == data)
 		return data->y();
 
-	auto p_below = p_above - 1; // get the position of value just below or equal to xVal
+	auto p_below = p_above - 1; // get the position of value just below or equal to x_val
 
-	if ((p_above == (data + lData)) || comp_equal(xVal, p_below->x())) // if last or p_below->x() == xVal
+	if ((p_above == (data + l_data)) || comp_equal(x_val, p_below->x())) // if last or p_below->x() == x_val
 		return p_below->y(); // same x so take y directly
 
 
 	// compute the interpolation	 y = y0 + (y1-y0)*(x-x0)/(x1-x0);
-	return p_below->y() + (p_above->y() - p_below->y()) * (xVal - p_below->x()) / (p_above->x() - p_below->x());
+	return p_below->y() + (p_above->y() - p_below->y()) * (x_val - p_below->x()) / (p_above->x() - p_below->x());
 }
 
 template <typename T>
-void linear_interpolation(const T* x_data, const T* y_data, const unsigned lData,
-                          const T* x_vals,       T* y_vals, const unsigned lVals)
+void linear_interpolation(const T* x_data, const T* y_data, const unsigned l_data,
+                          const T* x_vals,       T* y_vals, const unsigned l_vals)
 {
-	for(unsigned j = 0; j < lVals; j++)
-		y_vals[j] = linear_interpolation(x_data, y_data, lData, x_vals[j]);
+	for(unsigned j = 0; j < l_vals; j++)
+		y_vals[j] = linear_interpolation(x_data, y_data, l_data, x_vals[j]);
 }
 
 template <typename T>
