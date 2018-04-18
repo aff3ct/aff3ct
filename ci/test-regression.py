abe4e8bc3c628bd0592530c372069cff91fcc7a9
Olivier HARTMANN <olivier.hartmann@u-bordeaux.fr>
2018-04-18 17:30:51 +0200

    Modify test regression to handle several noise types and data to check

--- ci/test-regression.py
+++ ci/test-regression.py
@@ -10,6 +10,7 @@ import time
 import pathlib
 import argparse
 import subprocess
+import numpy as np
 
 # ==================================================================== PACKAGES
 # =============================================================================
@@ -31,13 +32,20 @@ parser.add_argument('--max-snr-time',   action='store', dest='maxSNRTime',    ty
 parser.add_argument('--verbose',        action='store', dest='verbose',       type=bool,  default=False,                     help='Enable the verbose mode.')
 
 # supported file extensions (filename suffix)
-extensions = ['.txt', '.perf', '.data', '.dat']
+extensions    = ['.txt', '.perf', '.data', '.dat']
+NoiseTypeList = ["Eb/N0", "Es/N0", "ROP", "EP"]
+
 
 # ================================================================== PARAMETERS
 # =============================================================================
 
 # =============================================================================
 # =================================================================== FUNCTIONS
+def format_e(n):
+	a = '%.2e' % n
+	return a
+	# return a.split('E')[0].rstrip('0').rstrip('.') + 'E' + a.split('E')[1]
+
 
 def getFileNames(currentPath, fileNames):
 	if os.path.isdir(currentPath):
@@ -68,7 +76,296 @@ def getFileNames(currentPath, fileNames):
 	else:
 		print("# (WW) The path '", currentPath, "' does not exist.")
 
-# -----
+def getLegend(line):
+	line = line.replace("#", "")
+	line = line.replace("||", "|")
+	line = line.split('|')
+	for i in range(len(line)):
+		line[i] = line[i].strip()
+
+	return line
+
+def getVal(line):
+	line = line.replace("#", "")
+	line = line.replace("||", "|")
+	line = line.split('|')
+
+	valLine = []
+	for i in range(len(line)):
+		val = float(0.0)
+
+		try:
+			val = float(line[i])
+
+			if "inf" in str(val):
+				val = float(0.0)
+
+		except ValueError:
+			pass
+
+		valLine.append(val)
+
+	return valLine
+
+def getLegendIdx(legend, colName):
+	for i in range(len(legend)):
+		if legend[i] == colName:
+			return i
+	return -1
+
+class simuData:
+	def __init__(self):
+		self.Legend     = []
+		self.All        = []
+		self.Noise      = []
+		self.BER        = []
+		self.FER        = []
+		self.MI         = []
+		self.RunCommand = ""
+		self.CurveName  = ""
+		self.NoiseType  = ""
+
+def dataReader(aff3ctOutput):
+	data = simuData()
+
+	for line in aff3ctOutput:
+		if line.startswith("#"):
+			if len(line) > 20 and (line.find("FRA |") != -1 or line.find("BER |") != -1 or line.find("FER |") != -1):
+				data.Legend = getLegend(line)
+
+		else:
+			if len(data.Legend) != 0:
+				d = getVal(line)
+				if len(d) == len(data.Legend):
+					data.All.append(d)
+
+
+	data.All = np.array(data.All).transpose()
+
+	# get the command to to run to reproduce this trace
+	if len(aff3ctOutput) >= 2 and "Run command:" in aff3ctOutput[0]:
+		data.RunCommand = str(aff3ctOutput[1].strip())
+	elif len(aff3ctOutput) >= 4 and "Run command:" in aff3ctOutput[2]:
+		data.RunCommand = str(aff3ctOutput[3].strip())
+	else:
+		data.RunCommand = ""
+
+	# get the curve name (if there is one)
+	if len(lines) >= 2 and "Curve name:" in lines[0]:
+		data.CurveName = str(lines[1].strip())
+	elif len(lines) >= 4 and "Curve name:" in lines[2]:
+		data.CurveName = str(lines[3].strip())
+	else:
+		data.CurveName = ""
+
+
+	# find the type of noise used in this simulation
+	idx = -1
+	for n in NoiseTypeList:
+		idx = getLegendIdx(data.Legend, n)
+		if idx != -1:
+			data.NoiseType = n
+			break
+
+	if len(data.All) and idx != -1 :
+		# set noise range
+		data.Noise = data.All[idx]
+
+		# set BER
+		idx = getLegendIdx(data.Legend, "BER")
+		if idx == -1:
+			data.BER = [0 for x in range(len(data.Noise))]
+		else:
+			data.BER = data.All[idx]
+
+		# set FER
+		idx = getLegendIdx(data.Legend, "FER")
+		if idx == -1:
+			data.FER = [0 for x in range(len(data.Noise))]
+		else:
+			data.FER = data.All[idx]
+
+		# set MI
+		idx = getLegendIdx(data.Legend, "MI")
+		if idx == -1:
+			data.MI = []
+		else:
+			data.MI = data.All[idx]
+
+	return data
+
+def splitAsCommand(runCommand):
+	# split the run command
+	argsList = [""]
+	idx = 0
+
+	new = 0
+	found_dashes = 0
+	for s in runCommand:
+		if found_dashes == 0:
+			if s == ' ':
+				if new == 0:
+					argsList.append("")
+					idx = idx + 1
+					new = 1
+
+			elif s == '\"':
+				if new == 0:
+					argsList.append("")
+					idx = idx + 1
+					new = 1
+				found_dashes = 1
+
+			else:
+				argsList[idx] += s
+				new = 0
+
+		else: # between dashes
+			if s == '\"':
+				argsList.append("")
+				idx = idx + 1
+				found_dashes = 0
+
+			else:
+				argsList[idx] += s
+
+	del argsList[idx]
+
+	return argsList
+
+class tableStats:
+	def __init__(self, tableCur, tableRef, sensibility, name):
+		if len(tableCur) > len(tableRef) : # can't have less ref than checked points
+			raise RuntimeError
+
+		self.tableCur        = tableCur
+		self.tableRef        = tableRef
+		self.name            = name
+		self.sensibility     = sensibility
+		self.minSensibility  = 0
+		self.maxSensibility  = 0
+		self.sumSensibility  = 0
+		self.valid           = 0
+		self.nData           = len(tableCur)
+		self.errorsList      = []
+		self.errorsPos       = []
+		self.errorsMessage   = []
+
+		for i in range(self.nData):
+			diff = tableCur[i] - tableRef[i];
+			absoluteDiff = math.fabs(diff)
+
+			if absoluteDiff > self.sensibility:
+				self.errorsPos    .append(i)
+				self.errorsList   .append([tableCur[i], diff])
+				self.errorsMessage.append(format_e(tableRef[i]))
+			else:
+				self.valid += 1
+
+			self.sumSensibility += absoluteDiff
+			self.minSensibility  = min(self.minSensibility, absoluteDiff)
+			self.maxSensibility  = max(self.maxSensibility, absoluteDiff)
+
+		if self.nData != 0:
+			self.avgSensibility = self.sumSensibility / float(self.nData)
+		else:
+			self.avgSensibility = 0
+		self.rateSensibility = (self.avgSensibility / self.sensibility) * 100
+
+	def errorMessage(self, idx):
+		if len(self.errorsMessage) > idx and len(self.errorsMessage[idx]):
+			return self.name + "=" + self.errorsMessage[idx]
+		else:
+			return ""
+
+	def hasError(self):
+		return self.valid != self.nData
+
+	def passRate(self):
+		if self.nData == 0:
+			raise RuntimeError
+
+		return float(self.valid) / float(self.nData)
+
+class compStats:
+	def __init__(self, dataCur, dataRef, sensibility):
+		if not isinstance(dataCur, simuData) or not isinstance(dataRef, simuData) :
+			raise TypeError
+
+		self.dataCur  = dataCur
+		self.dataRef  = dataRef
+		self.dataList = []
+		self.dataList.append(tableStats(dataCur.FER, dataRef.FER, sensibility, "FER"))
+		self.dataList.append(tableStats(dataCur.BER, dataRef.BER, sensibility, "BER"))
+		self.dataList.append(tableStats(dataCur.MI,  dataRef.MI,  sensibility, "MI" ))
+
+	def errorMessage(self, idx):
+		message = ""
+		for d in self.dataList:
+			dm = d.errorMessage(idx)
+			if len(dm):
+				message += dm + " "
+
+		if len(message):
+			return "WRONG! " + message
+		else:
+			return ""
+
+	def getResumeTable(self):
+		message = ""
+		if len(self.dataCur.Noise) == 0:
+			return message
+
+		message += "---- Details: first noise point = " + format_e(self.dataCur.Noise[0]) + ", last noise point = " + format_e(self.dataCur.Noise[len(self.dataCur.Noise) -1]) + "\n"
+		message += "---- name | valid points |      sensibility [avg,min,max,rate%]      | [first,last] point \n"
+
+		for d in self.dataList:
+			if len(d.tableCur):
+				message += d.name.rjust(9) + " | "
+				message += (str(d.valid) + "/" + str(d.nData)).rjust(12) + " | "
+				message += ("[" + format_e(d.avgSensibility) + ", " + format_e(d.minSensibility) + ", " + format_e(d.maxSensibility) + ", " + format_e(d.rateSensibility) + "]").rjust(41) + " | "
+				message += ("[" + format_e(d.tableCur[0]) + "," + format_e(d.tableCur[len(d.tableCur) -1]) + "]").rjust(18) + "\n"
+
+		if self.hasError():
+			message += "---- name | errors list \n"
+			for d in self.dataList:
+				if d.hasError():
+					message += d.name.rjust(9) + " | "
+
+					el = 0
+					for e in d.errorsList:
+						message += "{" + str(e[0]) + " -> "
+						if e[1] > 0: message += "+"
+						message += format_e(e[1]) + "}"
+
+						el += 1
+						if el < len(d.errorsList):
+							message += ", "
+			message += "\n"
+
+		return message
+
+	def hasError(self):
+		hasError = False
+		for d in self.dataList:
+			hasError = hasError or d.hasError()
+		return hasError
+
+	def passRate(self):
+		passRate = float(0)
+
+		nGoodList = 0
+		for d in self.dataList:
+			try:
+				passRate += d.passRate()
+				nGoodList += 1
+			except RuntimeError:
+				pass
+
+		if nGoodList:
+			return passRate / float(nGoodList)
+		else:
+			return passRate
 
 # =================================================================== FUNCTIONS
 # =============================================================================
@@ -132,63 +429,18 @@ for fn in fileNames:
 	# open the file in read mode (from the fileName "fn" and the path)
 	f = open(args.refsPath + "/" + fn, 'r')
 
-	# read all the lines from the current file f
+	# read all the lines from the reference file f
 	lines = []
 	for line in f:
 		lines.append(line)
 
-	# get the main infos from the lines
-	runCommand = lines[1].strip()
-
-	currentSection = ""
-	idx = 0
-	simuRef = []
-	for l in lines:
-		# avoid the first lines and the comments
-		if idx > 6 and l.replace(" ", "") != ""   and \
-		               l.replace(" ", "") != "\n" and \
-		               l[0] != '#':
-			simuRef.append(l.strip().replace("||", "|").replace(" ", "").split("|"))
-		idx = idx +1
-
 	f.close()
 
-	# split the run command
-	argsAFFECT = [""]
-	idx = 0
+	# parse the reference file
+	simuRef = dataReader(lines)
 
-	new = 0
-	found_dashes = 0
-	for s in runCommand:
-		if found_dashes == 0:
-			if s == ' ':
-				if new == 0:
-					argsAFFECT.append("")
-					idx = idx + 1
-					new = 1
-
-			elif s == '\"':
-				if new == 0:
-					argsAFFECT.append("")
-					idx = idx + 1
-					new = 1
-				found_dashes = 1
-
-			else:
-				argsAFFECT[idx] += s
-				new = 0
-
-		else: # between dashes
-			if s == '\"':
-				argsAFFECT.append("")
-				idx = idx + 1
-				found_dashes = 0
-
-			else:
-				argsAFFECT[idx] += s
-
-	del argsAFFECT[idx]
-
+	# get the command line to run
+	argsAFFECT = splitAsCommand(simuRef.RunCommand)
 	argsAFFECT.append("--ter-freq")
 	argsAFFECT.append("0")
 	if args.maxFE:
@@ -201,6 +453,18 @@ for fn in fileNames:
 		argsAFFECT.append("--sim-stop-time")
 		argsAFFECT.append(str(args.maxSNRTime))
 
+	noiseVals = ""
+	for n in range(len(simuRef.Noise)):
+		if n != 0:
+			noiseVals += ",";
+
+		noiseVals += str(simuRef.Noise[n])
+
+	argsAFFECT.append("-R")
+	argsAFFECT.append(noiseVals)
+
+
+	# run the tested simulator
 	os.chdir(args.buildPath)
 	startTime = time.time()
 	processAFFECT = subprocess.Popen(argsAFFECT, stdout=subprocess.PIPE,
@@ -211,7 +475,16 @@ for fn in fileNames:
 
 	errAndWarnMessages = stderrAFFECT.decode(encoding='UTF-8')
 
+
+	# compare ref and nex results to check if the simulator is still OK
 	os.chdir(PathOrigin)
+
+	# get the results
+	stdOutput = stdoutAFFECT.decode(encoding='UTF-8').split("\n")
+	simuCur = dataReader(stdOutput)
+
+	# result file
+	fRes = open(args.resultsPath + "/" + fn, 'w+')
 
 	if returnCode:
 		print(" - ABORTED.", end="\n");
@@ -220,67 +493,50 @@ for fn in fileNames:
 			print(errAndWarnMessages)
 		nErrors = nErrors +1
 		failIds.append(testId +1)
-	else:
-		# begin to write the results into a file
-		fRes = open(args.resultsPath + "/" + fn, 'w+')
 
-		# parse the results
-		stdOutput = stdoutAFFECT.decode(encoding='UTF-8').split("\n")
-		outputAFFECTLines = []
-		simuCur = []
-		for l in stdOutput:
-			if l != "" and l[0] != '#' and l[0] != "(": # avoid the first lines and the comments
-				array = l.strip().replace("||", "|").replace(" ", "").split("|")
-				if (len(array) == 12 or len(array) == 9):
-					outputAFFECTLines.append(l)
-					simuCur.append(array)
-				else:
+	elif not simuCur.NoiseType == simuRef.NoiseType:
+		failIds.append(testId +1)
+		nErrors += 1
+		print(" - NOISE TYPE MISMATCH: " + simuRef.NoiseType + " vs " + simuCur.NoiseType + ".", end="\n");
+
+	else:
+		# parse the results to validate (or not) the BER/FER/MI performance
+		comp = compStats(simuCur, simuRef, args.sensibility)
+
+		# print the header
+		fRes.write("Run command:\n" + simuCur.RunCommand + "\n")
+		fRes.write("Curve name:\n"  + simuCur.CurveName  + "\n")
+
+		# print the parameters directly and add to the wrong data the ref values
+		idxNoise = 0
+		i = 0
+		while( i < len(stdOutput)):
+			l = stdOutput[i]
+			if l.startswith("#"):
+				if "# End of the simulation." not in l:
 					fRes.write(l + "\n")
-			elif l != "" and l[0] == '#' and "# End of the simulation." not in l:
-				fRes.write(l + "\n")
+
+			elif l == "Run command:" or l == "Curve name:":
+				i += 1 # ignore the following line
+
+			else:
+				em = comp.errorMessage(idxNoise)
+				fRes.write(l + em + "\n")
+				idxNoise += 1
+
+			i += 1 # to next line
+
 
 		fRes.flush()
 
-		# validate (or not) the BER/FER performance
-		valid = 0
-		idx = 0
-		errorsList = []
-		sensibilityList = []
-		for ref in simuRef:
-			try:
-				cur_fe = int(simuCur[idx][4])
-			except IndexError: # no such line
-				break
-
-			if cur_fe < args.maxFE / 2:
-				break
-
-			numRef = float(ref[6][0:4])
-			powerRef = int(ref[6][6:8])
-			numCur = float(simuCur[idx][6][0:4])
-			powerCur = int(simuCur[idx][6][6:8])
-
-			if powerRef - powerCur != 0:
-				if powerRef > powerCur:
-					numCur = numCur * 10**(math.fabs(powerCur - powerRef))
-				else:
-					numRef = numRef * 10**(math.fabs(powerCur - powerRef))
-
-			absoluteNumDiff = math.fabs(numCur - numRef)
-			sensibilityList.append(absoluteNumDiff)
-			if absoluteNumDiff > args.sensibility:
-				fRes.write(outputAFFECTLines[idx] + "WRONG! FER=" + ref[6][0:8] + "\n")
-				errorsList.append([float(simuCur[idx][1][0:4]), numCur - numRef])
-			else:
-				valid = valid + 1
-				fRes.write(outputAFFECTLines[idx] + "\n")
-
-			idx = idx + 1
-
+		# validate (or not) the BER/FER/MI performance
 		print(" - %.2f" %elapsedTime, "sec", end="")
-		if valid == idx:
+
+		passRate = comp.passRate()
+
+		if passRate == float(1):
 			print(" - STRONG PASSED.", end="\n");
-		elif idx != 0 and float(valid) / float(idx) >= args.weakRate:
+		elif passRate >= args.weakRate:
 			print(" - WEAK PASSED.", end="\n");
 		else:
 			print(" - FAILED.", end="\n");
@@ -288,42 +544,12 @@ for fn in fileNames:
 			failIds.append(testId +1)
 
 		if args.verbose:
-			avgSensibility = 0
-			minSensibility = 0
-			maxSensibility = 0
-			if idx == 0:
-				avgSensibility = 0
-			else:
-				avgSensibility = sum(sensibilityList) / float(idx)
-				minSensibility = min(sensibilityList)
-				maxSensibility = max(sensibilityList)
-			rateSensibility = (avgSensibility / args.sensibility) * 100
+			print (comp.getResumeTable())
 
-			print("---- Details: 'valid SNR points' = ", valid, "/", idx,
-			      ", 'sensibility [avg,min,max,rate]' = [ %.2f" %avgSensibility,
-			      ", %.2f" %minSensibility, ", %.2f" %maxSensibility,
-			      ", %.1f" % rateSensibility, "% ].", end="\n")
-			if idx > 0:
-				print("---- Details: 'first SNR point' =", float(simuCur[0][1][0:4]),
-				      "dB (@", simuCur[0][6][0:8],
-				      "FER), 'last SNR point' =", float(simuCur[idx -1][1][0:4]),
-				      "dB (@", simuCur[idx -1][6][0:8], "FER).")
-			if len(errorsList):
-				print("---- Details: 'errors list' = [", end="")
-				el = 0
-				for error in errorsList:
-					if error[1] > 0:
-						print("{", error[0], "dB -> +%.2f" %error[1], "}", end="")
-					else:
-						print("{", error[0], "dB -> %.2f" %error[1], "}", end="")
-					if el < len(errorsList) -1:
-						print(", ", end="")
-					el = el + 1
-				print("].", end="\n")
-
-		if errAndWarnMessages:
-			print("---- Warning message(s):", end="\n");
-			print(errAndWarnMessages)
+	if errAndWarnMessages:
+		print("---- Warning message(s):", end="\n");
+		print(errAndWarnMessages)
+		fRes.write(errAndWarnMessages)
 
 		fRes.write("# End of the simulation.\n")
 		fRes.close();
