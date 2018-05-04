613b98841d1140c0615e6875349d9fd3fc3a0766
Olivier HARTMANN <olivier.hartmann@u-bordeaux.fr>
2018-05-04 17:04:42 +0200

    Remove the reference to Sparse matrix H

--- src/Module/Decoder/LDPC/BP/Decoder_LDPC_BP.hpp
+++ src/Module/Decoder/LDPC/BP/Decoder_LDPC_BP.hpp
@@ -14,7 +14,7 @@ class Decoder_LDPC_BP : public Decoder_SISO_SIHO<B,R>
 {
 protected:
 	const int                   n_ite;
-	const tools::Sparse_matrix &H; // CN are along the columns -> _H.get_n_rows() == M (often M=N-K)
+	const tools::Sparse_matrix  H; // CN are along the columns -> _H.get_n_rows() == M (often M=N-K)
 	                               // VN are along the rows    -> _H.get_n_cols() == N
 	                               // automatically transpose in the constructor if needed
 	const bool                  enable_syndrome;
