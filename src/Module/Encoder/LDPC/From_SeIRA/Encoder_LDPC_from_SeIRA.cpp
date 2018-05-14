b62443f190086d65df3a61e6250c7ffaa9944b92
Olivier HARTMANN <olivier.hartmann@u-bordeaux.fr>
2018-05-14 16:33:12 +0200

    Add an Encoder LDPC for SeIRA matrices

--- src/Module/Encoder/LDPC/From_SeIRA/Encoder_LDPC_from_SeIRA.cpp
+++ src/Module/Encoder/LDPC/From_SeIRA/Encoder_LDPC_from_SeIRA.cpp
@@ -7,18 +7,17 @@
 #include "Tools/Exception/exception.hpp"
 #include "Tools/Math/matrix.h"
 
-#include "Encoder_LDPC_from_QC.hpp"
+#include "Encoder_LDPC_from_SeIRA.hpp"
 
 using namespace aff3ct;
 using namespace aff3ct::module;
 
 template <typename B>
-Encoder_LDPC_from_QC<B>
-::Encoder_LDPC_from_QC(const int K, const int N, const tools::Sparse_matrix &_H, const int n_frames)
-: Encoder_LDPC<B>(K, N, n_frames),
-  invH2(tools::LDPC_matrix_handler::invert_H2(_H))
+Encoder_LDPC_from_SeIRA<B>
+::Encoder_LDPC_from_SeIRA(const int K, const int N, const tools::Sparse_matrix &_H, const int n_frames)
+: Encoder_LDPC<B>(K, N, n_frames)
 {
-	const std::string name = "Encoder_LDPC_from_QC";
+	const std::string name = "Encoder_LDPC_from_SeIRA";
 	this->set_name(name);
 
 	this->H = _H;
@@ -27,7 +26,7 @@ Encoder_LDPC_from_QC<B>
 }
 
 template <typename B>
-void Encoder_LDPC_from_QC<B>
+void Encoder_LDPC_from_SeIRA<B>
 ::_encode(const B *U_K, B *X_N, const int frame_id)
 {
 	int M = this->N - this->K;
@@ -35,39 +34,42 @@ void Encoder_LDPC_from_QC<B>
 	//Systematic part
 	std::copy_n(U_K, this->K, X_N);
 
-	//Calculate parity part
-	mipp::vector<int8_t> parity(M, 0);
+	//init the parity part
+	B* parity = X_N + this->K;
+	std::fill_n(parity, M, 0);
 
-	for (auto i = 0; i < M; i++)
+
+	//Calculate parity part
+	for (auto& l : this->H.get_rows_from_col(0))
+		if (l < (unsigned)this->K)
+			parity[0] ^= U_K[l];
+
+	for (auto i = 1; i < M; i++)
+	{
+		parity[i] = parity[i -1];
+
 		for (auto& l : this->H.get_rows_from_col(i))
 			if (l < (unsigned)this->K)
 				parity[i] ^= U_K[l];
-
-	auto* X_N_ptr = X_N + this->K;
-	for (auto i = 0; i < M; i++)
-	{
-		X_N_ptr[i] = 0;
-		for (auto j = 0; j < M; j++)
-			X_N_ptr[i] ^= parity[j] & invH2[i][j];
 	}
 }
 
 template <typename B>
-const std::vector<uint32_t>& Encoder_LDPC_from_QC<B>
+const std::vector<uint32_t>& Encoder_LDPC_from_SeIRA<B>
 ::get_info_bits_pos() const
 {
 	return Encoder<B>::get_info_bits_pos();
 }
 
 template <typename B>
-bool Encoder_LDPC_from_QC<B>
+bool Encoder_LDPC_from_SeIRA<B>
 ::is_sys() const
 {
 	return Encoder<B>::is_sys();
 }
 
 template <typename B>
-void Encoder_LDPC_from_QC<B>
+void Encoder_LDPC_from_SeIRA<B>
 ::_check_H_dimensions()
 {
 	Encoder_LDPC<B>::_check_H_dimensions();
@@ -84,11 +86,11 @@ void Encoder_LDPC_from_QC<B>
 // ==================================================================================== explicit template instantiation
 #include "Tools/types.h"
 #ifdef MULTI_PREC
-template class aff3ct::module::Encoder_LDPC_from_QC<B_8>;
-template class aff3ct::module::Encoder_LDPC_from_QC<B_16>;
-template class aff3ct::module::Encoder_LDPC_from_QC<B_32>;
-template class aff3ct::module::Encoder_LDPC_from_QC<B_64>;
+template class aff3ct::module::Encoder_LDPC_from_SeIRA<B_8>;
+template class aff3ct::module::Encoder_LDPC_from_SeIRA<B_16>;
+template class aff3ct::module::Encoder_LDPC_from_SeIRA<B_32>;
+template class aff3ct::module::Encoder_LDPC_from_SeIRA<B_64>;
 #else
-template class aff3ct::module::Encoder_LDPC_from_QC<B>;
+template class aff3ct::module::Encoder_LDPC_from_SeIRA<B>;
 #endif
 // ==================================================================================== explicit template instantiation
