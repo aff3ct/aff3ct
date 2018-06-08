ef4d6cc314f492e42cb2b19b889cf1fd75fa0be5
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-06-08 11:52:31 +0200

    Merge build and test coverage scripts.

--- ci/test-build-coverage-regression.sh
+++ ci/test-build-coverage-regression.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -x
 
+THREADS=$(grep -c ^processor /proc/cpuinfo)
 WD=$(pwd)
 build_root=build_coverage_linux_x86_gcc
 
-cd ${build_root}
-tar -xvvf CMakeFiles.tar.gz
-cd ..
+function compile {
+	build=$1
+	mkdir $build
+	cd $build
+	cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS_DEBUG="-O0" -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -msse4.2 -DMULTI_PREC -DENABLE_COOL_BASH --coverage" -DCMAKE_EXE_LINKER_FLAGS="--coverage"
+	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+	make -j $THREADS
+	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+	cd ..
+}
 
 function gen_coverage_info
 {
@@ -28,12 +36,14 @@ function gen_coverage_info
 	done
 }
 
+compile "${build_root}"
+cd ${WD}
 gen_coverage_info "${build_root}" "refs"
+cd ${WD}
 
-pwd=$(pwd)
 mkdir code_coverage_files || true
 lcov --capture --directory $build_root/CMakeFiles/aff3ct-obj.dir/src --output-file code_coverage_files/aff3ct.info
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-lcov --remove code_coverage_files/aff3ct.info "*/usr*" "*lib/*" "*/Tools/version.cpp" --output-file $pwd/code_coverage_files/aff3ct_clean.info
+lcov --remove code_coverage_files/aff3ct.info "*/usr*" "*lib/*" "*/Tools/version.cpp" --output-file ${WD}/code_coverage_files/aff3ct_clean.info
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 sed -i -e "s#${WD}/#\./#g" code_coverage_files/aff3ct_clean.info
