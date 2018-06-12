86ce51c44e0dea5478f43506f3edc304aae25062
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-06-12 16:51:48 +0200

    Try to fix CI issues.

--- ci/build-linux-macosx.sh
+++ ci/build-linux-macosx.sh
@@ -6,8 +6,8 @@ source ci/threads.sh
 mkdir build
 cd build
 
-cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=$CC \
-         -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -DENABLE_COOL_BASH $FLAGS" \
+cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX \
+         -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -DENABLE_COOL_BASH $CFLAGS" \
          -DENABLE_EXEC=ON -DENABLE_STATIC_LIB=ON -DENABLE_SHARED_LIB=ON $OPT
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
