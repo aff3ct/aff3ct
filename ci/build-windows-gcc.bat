b0f040ddec75ee7516eff664fc4e567605a707dd
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-06-12 21:14:19 +0200

    Try to fix batch scripts.

--- ci/build-windows-gcc.bat
+++ ci/build-windows-gcc.bat
@@ -6,7 +6,9 @@ set PATH=%PATH%;C:\Program Files\Git\mingw64\bin
 mkdir build
 cd build
 cmake .. -G"MinGW Makefiles" -DCMAKE_CXX_COMPILER=g++.exe -DCMAKE_BUILD_TYPE=Release %CMAKE_OPT% -DCMAKE_CXX_FLAGS="-Wall -Wno-misleading-indentation -funroll-loops %CFLAGS%" -DCMAKE_EXE_LINKER_FLAGS="%LFLAGS%"
+if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
 mingw32-make
+if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
 
 mkdir %NAME%
 mkdir %NAME%\bin\
--- ci/build-windows-msvc.bat
+++ ci/build-windows-msvc.bat
@@ -8,7 +8,9 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary
 mkdir build
 cd build
 cmake .. -G"Visual Studio 15 2017 Win64" %CMAKE_OPT% -DCMAKE_CXX_FLAGS="-D_CRT_SECURE_NO_DEPRECATE /MT /EHsc %CFLAGS%"
+if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
 devenv /build Release aff3ct.sln
+if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
 
 mkdir %NAME%
 mkdir %NAME%\bin\
