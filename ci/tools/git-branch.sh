30988f5ca51eb17515c335e22934f4c713ca7d6b
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-06-13 14:52:11 +0200

    Add an automatic script to deploy the builds on the AFF3CT web site.

---
+++ ci/tools/git-branch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# set -x
+
+GIT_BRANCH=$(git show -s --pretty=%d HEAD | sed -rne 's/.*origin\/(.*),.*/\1/p')
+if [ -z "$GIT_BRANCH" ]
+then
+	GIT_BRANCH=$(git show -s --pretty=%d HEAD | sed -rne 's/.*origin\/(.*)\)$/\1/p')
+fi
+
+if [ -z "$GIT_BRANCH" ]
+then
+	export GIT_BRANCH
+fi
