d4bb66c0a964b89a15841d7aaabe044a17b0afc6
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-06-14 17:49:28 +0200

    Try to fix the git-branch.sh script for real...

--- ci/tools/git-branch.sh
+++ ci/tools/git-branch.sh
@@ -7,6 +7,11 @@ then
 	GIT_BRANCH=$(git show -s --pretty=%d HEAD | sed -rne 's/.*origin\/(.*)\)$/\1/p')
 fi
 
+if [ -z "$GIT_BRANCH" ]
+then
+	GIT_BRANCH=$(git branch --remote --verbose --no-abbrev --contains | sed -rne 's/^[^\/]*\/([^\ ]+).*$/\1/p')
+fi
+
 if [ ! -z "$GIT_BRANCH" ]
 then
 	export GIT_BRANCH
