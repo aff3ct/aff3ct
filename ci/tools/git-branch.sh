843182a9d8eb371bf91fc6f1aeec657cdda07e82
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-06-15 18:09:49 +0200

    Force the 'GIT_BRANCH' value to 'master'.

--- ci/tools/git-branch.sh
+++ ci/tools/git-branch.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # set -x
 
-GIT_BRANCH=$(git show -s --pretty=%d HEAD | sed -rne 's/.*origin\/(.*),.*/\1/p')
-if [ -z "$GIT_BRANCH" ]
-then
-	GIT_BRANCH=$(git show -s --pretty=%d HEAD | sed -rne 's/.*origin\/(.*)\)$/\1/p')
-fi
+# GIT_BRANCH=$(git show -s --pretty=%d HEAD | sed -rne 's/.*origin\/(.*),.*/\1/p')
+# if [ -z "$GIT_BRANCH" ]
+# then
+# 	GIT_BRANCH=$(git show -s --pretty=%d HEAD | sed -rne 's/.*origin\/(.*)\)$/\1/p')
+# fi
 
-if [ -z "$GIT_BRANCH" ]
-then
-	GIT_BRANCH=$(git branch --remote --verbose --no-abbrev --contains | sed -rne 's/^[^\/]*\/([^\ ]+).*$/\1/p')
-fi
+# if [ -z "$GIT_BRANCH" ]
+# then
+# 	GIT_BRANCH=$(git branch --remote --verbose --no-abbrev --contains | sed -rne 's/^[^\/]*\/([^\ ]+).*$/\1/p')
+# fi
 
-if [ ! -z "$GIT_BRANCH" ]
-then
-	export GIT_BRANCH
-fi
+# if [ ! -z "$GIT_BRANCH" ]
+# then
+# 	export GIT_BRANCH
+# fi
+
+export GIT_BRANCH=master
