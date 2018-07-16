01c7b4fee819572ea1e4ec4eca69d4af72ba4df2
Mathieu Léonardon <mathieu.leonardon@u-bordeaux.fr>
2018-07-16 09:25:43 +0200

    Implement BEC frozen bits generator.

--- src/Tools/Code/Polar/Frozenbits_generator/Frozenbits_generator_BEC.cpp
+++ src/Tools/Code/Polar/Frozenbits_generator/Frozenbits_generator_BEC.cpp
@@ -31,26 +31,27 @@ void Frozenbits_generator_BEC
 		this->best_channels[i] = i;
 
 	for (auto i = 0; i < std::exp2(m); i++)
-		z[i] = 2.0 / std::pow((double)this->n->get_noise(), 2.0);
+		z[i] = static_cast<double>(this->n->get_noise());
 
 	for (auto l = 1; l <= m; l++)
 	{
 		auto o1 = (int)std::exp2(m - l +1);
 		auto o2 = (int)std::exp2(m - l   );
 
-//		for (auto t = 0; t < (int)std::exp2(l -1); t++)
-//		{
-//			double T = z[t * o1];
-//
-//			z[t * o1] = phi_inv(1.0 - std::pow(1.0 - phi(T), 2.0));
-//			if (z[t * o1] == HUGE_VAL)
-//				z[t * o1] = T + M_LN2 / (alpha * gamma);
-//
-//			z[t * o1 + o2] = 2.0 * T;
-//		}
+		for (auto t = 0; t < (int)std::exp2(l -1); t++)
+		{
+			double T = z[t * o1];
+
+			z[t * o1] = T * T;
+
+			z[t * o1 + o2] = 2 * T - T * T ;
+		}
 	}
 
-	std::sort(this->best_channels.begin(), this->best_channels.end(), [this](int i1, int i2) { return z[i1] > z[i2]; });
+//	std::sort(this->best_channels.begin(), this->best_channels.end(), [this](int i1, int i2) { return z[i1] > z[i2]; });
+//	for (auto chan : this->best_channels)
+//		std::cout << chan << " ";
+//	std::cout << std::endl;
 }
 
 void Frozenbits_generator_BEC
