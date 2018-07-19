63e7fcc8b9fb2c835822fda354bee4322bf6962b
Olivier HARTMANN <olivier.hartmann@u-bordeaux.fr>
2018-07-19 17:05:46 +0200

    Fix reporter exit linker error

--- src/Tools/Display/Reporter/EXIT/Reporter_EXIT.cpp
+++ src/Tools/Display/Reporter/EXIT/Reporter_EXIT.cpp
@@ -72,8 +72,6 @@ Reporter::report_t Reporter_EXIT<B,R>
 // ==================================================================================== explicit template instantiation
 #include "Tools/types.h"
 #ifdef MULTI_PREC
-template class aff3ct::tools::Reporter_EXIT<B_8 , R_8 >;
-template class aff3ct::tools::Reporter_EXIT<B_16, R_16>;
 template class aff3ct::tools::Reporter_EXIT<B_32, R_32>;
 template class aff3ct::tools::Reporter_EXIT<B_64, R_64>;
 #else
