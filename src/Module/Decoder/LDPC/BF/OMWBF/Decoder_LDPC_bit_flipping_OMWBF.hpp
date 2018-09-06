e25da7ac7c4494c209f1258f74a42e00644aa104
Vincent PIGNOLY <vincent.pignoly@gmail.com>
2018-09-06 17:19:33 +0200

    Add WBF and MWBF fit flipping decoders for LDPC codes

---
+++ src/Module/Decoder/LDPC/BF/OMWBF/Decoder_LDPC_bit_flipping_OMWBF.hpp
@@ -0,0 +1,39 @@
+#ifndef DECODER_LDPC_BIT_FLIPPING_OMWBF_HPP_
+#define DECODER_LDPC_BIT_FLIPPING_OMWBF_HPP_
+
+#include "../Decoder_LDPC_bit_flipping.hpp"
+
+namespace aff3ct
+{
+namespace module
+{
+template <typename B = int, typename R = float>
+class Decoder_LDPC_bit_flipping_OMWBF : public Decoder_LDPC_bit_flipping<B,R>
+{
+//private:
+//	const float normalize_factor;
+//	const R offset;
+
+public:
+	Decoder_LDPC_bit_flipping_OMWBF(const int &K, const int &N, const int& n_ite,
+	                              const tools::Sparse_matrix &H,
+	                              const std::vector<unsigned> &info_bits_pos,
+//	                              const float normalize_factor = 1.f,
+	                              const R mwbf_factor = (R)0,
+	                              const bool enable_syndrome = true,
+	                              const int syndrome_depth = 1,
+	                              const int n_frames = 1,
+	                              const std::string name = "Decoder_LDPC_bit_flipping_OMWBF");
+	virtual ~Decoder_LDPC_bit_flipping_OMWBF();
+
+protected:
+	// BF functions for decoding
+	virtual bool BF_process(const R *Y_N, std::vector<R> &V_to_C, std::vector<R> &C_to_V, const R *Y_min, short *decis);
+};
+
+template <typename B = int, typename R = float>
+using Decoder_LDPC_bit_flipping_OMWBF = Decoder_LDPC_bit_flipping_OMWBF<B,R>;
+}
+}
+
+#endif /* DECODER_LDPC_BIT_FLIPPING_OMWBF_HPP_ */
