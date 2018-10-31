0622f90d8e061756537f1e74c2e8828caf55b4c8
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-10-31 18:17:52 +0100

    Fix old school style CMake options.

--- ci/build-linux-macos.sh
+++ ci/build-linux-macos.sh
@@ -31,14 +31,14 @@ fi
 if [ -z "$LFLAGS" ]
 then
 	cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX \
-	         -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -DENABLE_COOL_BASH $CFLAGS" \
-	         -DENABLE_EXEC=ON -DENABLE_STATIC_LIB=ON -DENABLE_SHARED_LIB=ON $CMAKE_OPT
+	         -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops $CFLAGS" \
+	         $CMAKE_OPT
 	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 else
 	cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX \
-	         -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -DENABLE_COOL_BASH $CFLAGS" \
+	         -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops $CFLAGS" \
 	         -DCMAKE_EXE_LINKER_FLAGS="$LFLAGS" \
-	         -DENABLE_EXEC=ON -DENABLE_STATIC_LIB=ON -DENABLE_SHARED_LIB=ON $CMAKE_OPT
+	         $CMAKE_OPT
 	rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 fi
 
