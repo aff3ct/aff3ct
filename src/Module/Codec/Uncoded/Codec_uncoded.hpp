5caa3b6d6dab2da5510f17f07c1ba37611cef753
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-12-04 13:11:42 +0100

    Rm the decoder NO from the generic encoders.

--- src/Module/Codec/Uncoded/Codec_uncoded.hpp
+++ src/Module/Codec/Uncoded/Codec_uncoded.hpp
@@ -1,7 +1,7 @@
 #ifndef CODEC_UNCODED_HPP_
 #define CODEC_UNCODED_HPP_
 
-#include "Factory/Module/Encoder/Encoder.hpp"
+#include "Factory/Module/Encoder/NO/Encoder_NO.hpp"
 #include "Factory/Module/Decoder/NO/Decoder_NO.hpp"
 
 #include "../Codec_SISO_SIHO.hpp"
@@ -14,7 +14,7 @@ template <typename B = int, typename Q = float>
 class Codec_uncoded : public Codec_SISO_SIHO<B,Q>
 {
 public:
-	Codec_uncoded(const factory::Encoder   ::parameters &enc_params,
+	Codec_uncoded(const factory::Encoder_NO::parameters &enc_params,
 	              const factory::Decoder_NO::parameters &dec_params);
 	virtual ~Codec_uncoded() = default;
 
