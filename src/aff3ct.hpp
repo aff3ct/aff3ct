dda9fb27a8ebf2bbdf31339b94502b5bc21a910a
Olivier HARTMANN <olivier.hartmann@u-bordeaux.fr>
2018-12-12 17:11:10 +0100

    Update aff3ct header

--- src/aff3ct.hpp
+++ src/aff3ct.hpp
@@ -1282,29 +1282,20 @@
 #ifndef SELF_CORRECTED_HPP
 #include <Tools/Code/Turbo/Post_processing_SISO/Self_corrected/Self_corrected.hpp>
 #endif
-#ifndef CONSTELLATION_COMPLEX_HPP__
-#include <Tools/Constellation/Complex/Constellation_complex.hpp>
-#endif
-#ifndef CONSTELLATION_PSK_HPP__
-#include <Tools/Constellation/Complex/PSK/Constellation_PSK.hpp>
-#endif
-#ifndef CONSTELLATION_QAM_HPP__
-#include <Tools/Constellation/Complex/QAM/Constellation_QAM.hpp>
-#endif
-#ifndef CONSTELLATION_COMPLEX_USER_HPP__
-#include <Tools/Constellation/Complex/User/Constellation_complex_user.hpp>
-#endif
 #ifndef CONSTELLATION_HPP__
 #include <Tools/Constellation/Constellation.hpp>
 #endif
-#ifndef CONSTELLATION_REAL_HPP__
-#include <Tools/Constellation/Real/Constellation_real.hpp>
-#endif
 #ifndef CONSTELLATION_PAM_HPP__
-#include <Tools/Constellation/Real/PAM/Constellation_PAM.hpp>
+#include <Tools/Constellation/PAM/Constellation_PAM.hpp>
 #endif
-#ifndef CONSTELLATION_REAL_USER_HPP__
-#include <Tools/Constellation/Real/User/Constellation_real_user.hpp>
+#ifndef CONSTELLATION_PSK_HPP__
+#include <Tools/Constellation/PSK/Constellation_PSK.hpp>
+#endif
+#ifndef CONSTELLATION_QAM_HPP__
+#include <Tools/Constellation/QAM/Constellation_QAM.hpp>
+#endif
+#ifndef CONSTELLATION_USER_HPP__
+#include <Tools/Constellation/User/Constellation_user.hpp>
 #endif
 #ifndef DUMPER_HPP_
 #include <Tools/Display/Dumper/Dumper.hpp>
