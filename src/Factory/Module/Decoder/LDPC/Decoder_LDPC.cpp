04a6cb8e070f05d2ad0241f2774c6793232a26d3
Olivier HARTMANN <olivier.hartmann@u-bordeaux.fr>
2018-12-14 17:59:01 +0100

    Add the Gallager E, that is the Gallager B with extended alphabet.

--- src/Factory/Module/Decoder/LDPC/Decoder_LDPC.cpp
+++ src/Factory/Module/Decoder/LDPC/Decoder_LDPC.cpp
@@ -30,6 +30,7 @@
 #include "Module/Decoder/LDPC/BP/Horizontal_layered/ONMS/Decoder_LDPC_BP_horizontal_layered_ONMS_inter.hpp"
 #include "Module/Decoder/LDPC/BP/Flooding/Gallager/Decoder_LDPC_BP_flooding_Gallager_A.hpp"
 #include "Module/Decoder/LDPC/BP/Flooding/Gallager/Decoder_LDPC_BP_flooding_Gallager_B.hpp"
+#include "Module/Decoder/LDPC/BP/Flooding/Gallager/Decoder_LDPC_BP_flooding_Gallager_E.hpp"
 #include "Module/Decoder/LDPC/BP/Flooding/SPA/Decoder_LDPC_BP_flooding_SPA.hpp"
 #include "Module/Decoder/LDPC/BP/Peeling/Decoder_LDPC_BP_peeling.hpp"
 #include "Module/Decoder/LDPC/BF/OMWBF/Decoder_LDPC_bit_flipping_OMWBF.hpp"
@@ -76,7 +77,7 @@ void Decoder_LDPC::parameters
 #ifdef __cpp_aligned_new
 	tools::add_options(args.at({p+"-type", "D"}), 0, "BP_HORIZONTAL_LAYERED_LEGACY");
 #endif
-	tools::add_options(args.at({p+"-implem"   }), 0, "SPA", "LSPA", "MS", "OMS", "NMS", "AMS", "GALA", "GALB", "WBF");
+	tools::add_options(args.at({p+"-implem"   }), 0, "SPA", "LSPA", "MS", "OMS", "NMS", "AMS", "GALA", "GALB", "GALE", "WBF");
 
 	tools::add_arg(args, p, class_name+"p+ite,i",
 		tools::Integer(tools::Positive()));
@@ -368,6 +369,7 @@ module::Decoder_SIHO<B,Q>* Decoder_LDPC::parameters
 		{
 			if (this->implem == "GALA") return new module::Decoder_LDPC_BP_flooding_GALA<B,Q>(this->K, this->N_cw, this->n_ite, H, info_bits_pos, this->enable_syndrome, this->syndrome_depth, this->n_frames);
 			if (this->implem == "GALB") return new module::Decoder_LDPC_BP_flooding_GALB<B,Q>(this->K, this->N_cw, this->n_ite, H, info_bits_pos, this->enable_syndrome, this->syndrome_depth, this->n_frames);
+			if (this->implem == "GALE") return new module::Decoder_LDPC_BP_flooding_GALE<B,Q>(this->K, this->N_cw, this->n_ite, H, info_bits_pos, this->enable_syndrome, this->syndrome_depth, this->n_frames);
 		}
 		else if (this->type == "BP_PEELING")
 		{
