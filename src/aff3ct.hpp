b60fe460fbc7571d4f5b268e19314975e4617f6a
Olivier HARTMANN <olivier.hartmann@u-bordeaux.fr>
2018-12-18 09:18:33 +0100

    update aff3ct header

--- src/aff3ct.hpp
+++ src/aff3ct.hpp
@@ -1423,6 +1423,9 @@
 #ifndef INTERPOLATION_H_
 #include <Tools/Math/interpolation.h>
 #endif
+#ifndef MATH_CONSTANTS_H
+#include <Tools/Math/math_constants.h>
+#endif
 #ifndef MATRIX_H
 #include <Tools/Math/matrix.h>
 #endif
