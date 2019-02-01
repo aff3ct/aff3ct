d2632719c54fc3e2633bc2ebe58c53209e362ea3
Mathieu Léonardon <mathieu.leonardon@gmail.com>
2019-02-01 15:50:43 +0100

    CI: Fixed channel noise in debug_parser test for float hex values.

--- scripts/debug_parser/aff3ct_debug_parser.py
+++ scripts/debug_parser/aff3ct_debug_parser.py
@@ -226,7 +226,7 @@ class OutputStructure:
 
 def check_positive(value):
     ivalue = int(value)
-    if ivalue <= 0:
+    if ivalue < 0:
         raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
     return ivalue
 
--- scripts/debug_parser/test/test.sh
+++ scripts/debug_parser/test/test.sh
@@ -10,7 +10,7 @@ mkdir data/test4/
 
 if [ -z "$AFF3CT_PATH" ]
 then
-	AFF3CT_PATH=../../../build/bin/aff3ct
+	AFF3CT_PATH=../../../build/bin/aff3ct-2.2.2-20-g9de7e78
 fi
 
 $AFF3CT_PATH -p "8" --sim-type "BFER" -C "POLAR" -K "32" -N "64" -m "2.81"     \
@@ -26,13 +26,13 @@ $AFF3CT_PATH --sim-type "BFER" -p "8" --sim-cde-type "UNCODED" -K "12" -m "5"  \
 > dump_debug_gold2.txt
 
 $AFF3CT_PATH --sim-type "BFER" -p "8" --sim-cde-type "UNCODED" -K "12" -m "5"  \
--M "5" -s "0.5" --chn-type "RAYLEIGH" -t "1" --mdm-type "SCMA" --mdm-ite "4"   \
+-M "5" -s "0.5" --chn-type "USER" --chn-path "./refs/error_tracker_0.23.chn" -t "1" --mdm-type "SCMA" --mdm-ite "4"   \
 --mnt-max-fe "100" -F "6" --sim-dbg-hex --sim-meta "Dump Test"                 \
---mdm-codebook "conf/mod/SCMA/CS1.cb"                                          \
+--mdm-codebook "conf/mod/SCMA/CS1.cb"  \
 > dump_debug_gold4.txt
 
 rc=0
-# Test 1...
+echo "Test 1: single frame, int values."
 ../aff3ct_debug_parser.py ./dump_debug_gold.txt                                \
 --mod Decoder_polar_SC_fast_sys --tsk decode_siho --src --txt --bin --mat
 cmp Y_N.txt refs/Y_N_gold.txt                      ;rc=$(($?+$rc))
@@ -45,9 +45,9 @@ cmp Y_N.mat refs/Y_N_gold.mat -i 70                ;rc=$(($?+$rc))
 cmp V_K.mat refs/V_K_gold.mat -i 70                ;rc=$(($?+$rc))
 mv Y_N.txt V_K.txt Y_N.bin V_K.bin Y_N.h V_K.h      data/test1
 mv Y_N.mat V_K.mat                                  data/test1
-# Test 1 Done.
+echo "Test 1 Done."
 
-# Test 2...
+echo "Test 2: inter frames, int & float values."
 ../aff3ct_debug_parser.py ./dump_debug_gold2.txt                               \
 --mod Channel_Rayleigh_LLR --tsk add_noise_wg --src --txt --bin --mat
 cmp Y_N.txt refs/Y_N_gold2.txt                     ;rc=$(($?+$rc))
@@ -64,9 +64,9 @@ cmp X_N.mat refs/X_N_gold2.mat -i 70               ;rc=$(($?+$rc))
 cmp H_N.mat refs/H_N_gold2.mat -i 70               ;rc=$(($?+$rc))
 mv Y_N.txt X_N.txt H_N.txt Y_N.bin X_N.bin H_N.bin  data/test2
 mv Y_N.h X_N.h H_N.h Y_N.mat X_N.mat H_N.mat        data/test2
-# Test 2 Done.
+echo "Test 2 Done."
 
-# Test 3...
+echo "Test 3: single frame, int & float values."
 ../aff3ct_debug_parser.py ./dump_debug_gold.txt                                \
 --mod Quantizer_pow2 --tsk process --src --txt --bin --mat
 cmp Y_N1.txt refs/Y_N1_gold3.txt                   ;rc=$(($?+$rc))
@@ -79,24 +79,24 @@ cmp Y_N1.mat refs/Y_N1_gold3.mat -i 70             ;rc=$(($?+$rc))
 cmp Y_N2.mat refs/Y_N2_gold3.mat -i 70             ;rc=$(($?+$rc))
 mv Y_N1.txt Y_N2.txt Y_N1.bin Y_N2.bin              data/test3
 mv Y_N1.h Y_N2.h Y_N1.mat Y_N2.mat                  data/test3
-# Test 3 Done.
+echo "Test 3 Done."
 
-# Test 4...
+echo "Test 4: extract one frame (0) from inter frame, float hex values."
 ../aff3ct_debug_parser.py ./dump_debug_gold4.txt                               \
---mod Quantizer_pow2 --tsk process --src --txt --bin --mat --fra 3
-cmp Y_N1.txt refs/Y_N1_gold4.txt                   ;rc=$(($?+$rc))
-cmp Y_N2.txt refs/Y_N2_gold4.txt                   ;rc=$(($?+$rc))
-cmp Y_N1.bin refs/Y_N1_gold4.bin                   ;rc=$(($?+$rc))
-cmp Y_N2.bin refs/Y_N2_gold4.bin                   ;rc=$(($?+$rc))
-cmp Y_N1.h   refs/Y_N1_gold4.h                     ;rc=$(($?+$rc))
-cmp Y_N2.h   refs/Y_N2_gold4.h                     ;rc=$(($?+$rc))
-cmp Y_N1.mat refs/Y_N1_gold4.mat -i 70             ;rc=$(($?+$rc))
-cmp Y_N2.mat refs/Y_N2_gold4.mat -i 70             ;rc=$(($?+$rc))
-mv Y_N1.txt Y_N2.txt Y_N1.bin Y_N2.bin              data/test4
-mv Y_N1.h Y_N2.h Y_N1.mat Y_N2.mat                  data/test4
-# Test 4 Done.
+--mod Channel_user --tsk add_noise --src --txt --bin --mat --fra 0
+cmp X_N.txt refs/X_N_gold4.txt                   ;rc=$(($?+$rc))
+cmp Y_N.txt refs/Y_N_gold4.txt                   ;rc=$(($?+$rc))
+cmp X_N.bin refs/X_N_gold4.bin                   ;rc=$(($?+$rc))
+cmp Y_N.bin refs/Y_N_gold4.bin                   ;rc=$(($?+$rc))
+cmp X_N.h   refs/X_N_gold4.h                     ;rc=$(($?+$rc))
+cmp Y_N.h   refs/Y_N_gold4.h                     ;rc=$(($?+$rc))
+cmp X_N.mat refs/X_N_gold4.mat -i 70             ;rc=$(($?+$rc))
+cmp Y_N.mat refs/Y_N_gold4.mat -i 70             ;rc=$(($?+$rc))
+mv X_N.txt Y_N.txt X_N.bin Y_N.bin              data/test4
+mv X_N.h Y_N.h X_N.mat Y_N.mat                  data/test4
+echo "Test 4 Done."
 
-rm dump_debug_gold.txt dump_debug_gold2.txt dump_debug_gold4.txt
+# rm dump_debug_gold.txt dump_debug_gold2.txt dump_debug_gold4.txt
 
 cd .. > /dev/null
 
