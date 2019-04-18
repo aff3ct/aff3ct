998dfb9a07b7189395e8d100d7ff1cd3e1f3b621
Adrien Cassagne <adrien.cassagne@inria.fr>
2019-04-18 11:30:43 +0200

    Rm useless include + cosmetics.

--- src/Tools/Code/Polar/decoder_polar_functions.h
+++ src/Tools/Code/Polar/decoder_polar_functions.h
@@ -7,7 +7,6 @@
 #include <functional>
 
 #include "Tools/Math/utils.h"
-#include "Tools/Code/Polar/Polar_code.hpp"
 
 #ifndef _MSC_VER
 #ifndef __forceinline
@@ -131,18 +130,16 @@ struct Polar_lambdas
 };
 
 template <typename R>
-using proto_xor = R (*)(const R& lambda_a, const R& lambda_b);
+using proto_xor = R (*)(const R& ll, const R& lr);
 
 template <typename R>
-using proto_plus = R (*)(const R& lambda_a, const R& lambda_b);
-
-
-template <typename R>
-__forceinline R square_plus(const R& lambda_a, const R& lambda_b);
+using proto_plus = R (*)(const R& ll, const R& lr);
 
 template <typename R>
-__forceinline R plus(const R& lambda_a, const R& lambda_b);
+__forceinline R square_plus(const R& ll, const R& lr);
 
+template <typename R>
+__forceinline R plus(const R& ll, const R& lr);
 
 template <typename B, typename R, proto_xor <R> X = square_plus,
                                   proto_plus<R> P = plus>
