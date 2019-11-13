18f1d5131d51615a9d58678413f2ca8a360dae71
Adrien Cassagne <adrien.cassagne@inria.fr>
2019-11-13 11:20:42 +0100

    Improve the 'Callback' class by adding thread safety.

--- include/Tools/Algo/Callback/Callback.hpp
+++ include/Tools/Algo/Callback/Callback.hpp
@@ -5,6 +5,7 @@
 #ifndef CALLBACK_HPP__
 #define CALLBACK_HPP__
 
+#include <mutex>
 #include <vector>
 #include <cstdint>
 #include <utility>
@@ -19,15 +20,17 @@ class Callback
 {
 protected:
 	std::vector<std::pair<std::function<void(Args...)>, uint32_t>> callbacks;
+	mutable std::mutex mtx;
 
 public:
 	Callback() = default;
+	Callback(const Callback &c);
 	virtual ~Callback() = default;
 
 	uint32_t record(std::function<void(Args...)> callback);
 	bool unrecord(const uint32_t id);
 	void clear();
-	void notify(Args ... args) const;
+	void notify(Args ... args);
 };
 }
 }
--- include/Tools/Algo/Callback/Callback.hxx
+++ include/Tools/Algo/Callback/Callback.hxx
@@ -6,13 +6,24 @@ namespace tools
 {
 
 template <class... Args>
+Callback<Args...>
+::Callback(const Callback &c)
+{
+	c.mtx.lock();
+	this->callbacks = c.callbacks;
+	c.mtx.unlock();
+}
+
+template <class... Args>
 uint32_t Callback<Args...>
 ::record(std::function<void(Args...)> callback)
 {
+	this->mtx.lock();
 	uint32_t id = 0;
 	if (this->callbacks.size())
 		id = this->callbacks[this->callbacks.size() -1].second +1;
 	this->callbacks.push_back(std::make_pair(callback, id));
+	this->mtx.unlock();
 	return id;
 }
 
@@ -20,17 +31,20 @@ template <class... Args>
 bool Callback<Args...>
 ::unrecord(const uint32_t id)
 {
+	this->mtx.lock();
 	auto it = this->callbacks.begin();
 	while (it != this->callbacks.end())
 	{
 		if ((*it).second == id)
 		{
 			it = this->callbacks.erase(it);
+			this->mtx.unlock();
 			return true;
 		}
 		else
 			++it;
 	}
+	this->mtx.unlock();
 	return false;
 }
 
@@ -38,15 +52,19 @@ template <class... Args>
 void Callback<Args...>
 ::clear()
 {
+	this->mtx.lock();
 	this->callbacks.clear();
+	this->mtx.unlock();
 }
 
 template <class... Args>
 void Callback<Args...>
-::notify(Args ... args) const
+::notify(Args ... args)
 {
+	this->mtx.lock();
 	for (auto& c : this->callbacks)
 		c.first(args...);
+	this->mtx.unlock();
 }
 
 }
