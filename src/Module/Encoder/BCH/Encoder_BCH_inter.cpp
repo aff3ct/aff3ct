b09a7f97ade6b172281e1af92873ec30d103d9e8
Adrien Cassagne <adrien.cassagne@inria.fr>
2020-01-30 18:38:49 +0100

    Make the reordering process of the BCH encoder in the '__encode' method.

--- src/Module/Encoder/BCH/Encoder_BCH_inter.cpp
+++ src/Module/Encoder/BCH/Encoder_BCH_inter.cpp
@@ -33,31 +33,6 @@ template <typename B>
 void Encoder_BCH_inter<B>
 ::__encode(const B *U_K, B *par)
 {
-	std::fill(par, par + this->n_rdncy * mipp::N<B>(), (B)0);
-
-	mipp::Reg<B> reg_g_0 = this->g[0];
-	mipp::Reg<B> reg_zero = (B)0;
-
-	for (auto i = this->K -1; i >= 0; i--)
-	{
-		mipp::Reg<B> reg_par_n = &par[(this->n_rdncy -1) * mipp::N<B>()];
-		const auto reg_feedback = reg_par_n ^ &U_K[i * mipp::N<B>()];
-		for (auto j = this->n_rdncy -1; j > 0; j--)
-		{
-			mipp::Reg<B> reg_par_j_1 = &par[(j -1) * mipp::N<B>()];
-			auto reg_par_j = this->g[j] ? reg_par_j_1 ^ reg_feedback : reg_par_j_1;
-			reg_par_j.store(&par[j * mipp::N<B>()]);
-		}
-
-		auto reg_par_0 = mipp::blend(reg_g_0 & reg_feedback, reg_zero, reg_feedback == 1);
-		reg_par_0.store(&par[0 * mipp::N<B>()]);
-	}
-}
-
-template <typename B>
-void Encoder_BCH_inter<B>
-::_encode(const B *U_K, B *X_N, const int frame_id)
-{
 	constexpr int n_frames = mipp::N<B>();
 	std::vector<const B*> frames_in(n_frames);
 		for (auto f = 0; f < n_frames; f++)
@@ -65,19 +40,45 @@ void Encoder_BCH_inter<B>
 
 	tools::Reorderer_static<B,n_frames>::apply(frames_in, this->U_K_reordered.data(), this->K);
 
-	// generate the parity bits
-	this->__encode(this->U_K_reordered.data(), par_reordered.data());
+	std::fill(this->par_reordered.begin(), this->par_reordered.end(), (B)0);
+
+	mipp::Reg<B> reg_g_0 = this->g[0];
+	mipp::Reg<B> reg_zero = (B)0;
+
+	for (auto i = this->K -1; i >= 0; i--)
+	{
+		mipp::Reg<B> reg_par_n = &this->par_reordered[(this->n_rdncy -1) * mipp::N<B>()];
+		const auto reg_feedback = reg_par_n ^ &this->U_K_reordered[i * mipp::N<B>()];
+		for (auto j = this->n_rdncy -1; j > 0; j--)
+		{
+			mipp::Reg<B> reg_par_j_1 = &this->par_reordered[(j -1) * mipp::N<B>()];
+			auto reg_par_j = this->g[j] ? reg_par_j_1 ^ reg_feedback : reg_par_j_1;
+			reg_par_j.store(&this->par_reordered[j * mipp::N<B>()]);
+		}
+
+		auto reg_par_0 = mipp::blend(reg_g_0 & reg_feedback, reg_zero, reg_feedback == 1);
+		reg_par_0.store(&this->par_reordered[0 * mipp::N<B>()]);
+	}
 
 	std::vector<B*> frames_out(n_frames);
 	for (auto f = 0; f < n_frames; f++)
-		frames_out[f] = X_N + f * this->N;
+		frames_out[f] = par + f * this->N;
 	tools::Reorderer_static<B,n_frames>::apply_rev(this->par_reordered.data(), frames_out, this->n_rdncy);
+}
+
+template <typename B>
+void Encoder_BCH_inter<B>
+::_encode(const B *U_K, B *X_N, const int frame_id)
+{
+	// generate the parity bits
+	this->__encode(U_K, X_N);
 
 	// copy the sys bits at the end of the codeword
+	constexpr int n_frames = mipp::N<B>();
 	for (auto f = 0; f < n_frames; f++)
 		std::copy(U_K + (f +0) * this->K,
 		          U_K + (f +1) * this->K,
-	 	          X_N + (f +0) * this->N + this->n_rdncy);
+		          X_N + (f +0) * this->N + this->n_rdncy);
 }
 
 // ==================================================================================== explicit template instantiation
