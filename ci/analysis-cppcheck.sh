bc0911534d22d80069de4cd703b11cc151adc44d
Adrien Cassagne <adrien.cassagne@inria.fr>
2021-05-21 16:22:13 +0200

    CI: fix cppcheck script for version 1.9.

--- ci/analysis-cppcheck.sh
+++ ci/analysis-cppcheck.sh
@@ -10,28 +10,23 @@ find .\/src\/     -type f -follow -print | grep "[.]h$\|[.]hpp$\|[.]hxx$\|[.]cpp
 # cppcheck for sonarqube
 cppcheck --language=c++ --suppress=missingIncludeSystem --force --enable=all --std=c++11 -U_MSC_VER --file-list=src_files.txt --xml --xml-version=2 2> cppcheck/cppcheck.xml
 cppcheck --language=c++ --suppress=missingIncludeSystem --force --enable=all --std=c++11 -U_MSC_VER --file-list=src_files.txt 2> cppcheck/cppcheck_all.log
-cat cppcheck/cppcheck_all.log | grep "(error)"          > cppcheck/cppcheck_error.log
-cat cppcheck/cppcheck_all.log | grep "(warning)"        > cppcheck/cppcheck_warning.log
-cat cppcheck/cppcheck_all.log | grep "(performance)"    > cppcheck/cppcheck_performance.log
-cat cppcheck/cppcheck_all.log | grep "(style)"          > cppcheck/cppcheck_style.log
-cat cppcheck/cppcheck_all.log | grep "(portability)"    > cppcheck/cppcheck_portability.log
-cat cppcheck/cppcheck_all.log | grep "(information)"    > cppcheck/cppcheck_information.log
-cat cppcheck/cppcheck_all.log | grep "(unusedFunction)" > cppcheck/cppcheck_unusedFunction.log
-cat cppcheck/cppcheck_all.log | grep "(missingInclude)" > cppcheck/cppcheck_missingInclude.log
+cat cppcheck/cppcheck_all.log | grep "error:"          > cppcheck/cppcheck_error.log
+cat cppcheck/cppcheck_all.log | grep "warning:"        > cppcheck/cppcheck_warning.log
+cat cppcheck/cppcheck_all.log | grep "performance:"    > cppcheck/cppcheck_performance.log
+cat cppcheck/cppcheck_all.log | grep "style:"          > cppcheck/cppcheck_style.log
+cat cppcheck/cppcheck_all.log | grep "portability:"    > cppcheck/cppcheck_portability.log
+cat cppcheck/cppcheck_all.log | grep "information:"    > cppcheck/cppcheck_information.log
+cat cppcheck/cppcheck_all.log | grep "unusedFunction:" > cppcheck/cppcheck_unusedFunction.log
+cat cppcheck/cppcheck_all.log | grep "missingInclude:" > cppcheck/cppcheck_missingInclude.log
 
 COUNT=$(wc -l < cppcheck/cppcheck_error.log )
 
-if [ $COUNT -gt 1 ]; then
+if [ $COUNT -gt 0 ]; then
 	echo "Error count is $COUNT! cppcheck run failed :-(.";
 	echo ""
 	echo "Errors list:"
 	cat cppcheck/cppcheck_error.log
 	exit 1;
-elif [ $COUNT -gt 0 ]; then
-	echo "There is one false positive error.";
-	echo ""
-	echo "Errors list:"
-	cat cppcheck/cppcheck_error.log
 else
 	echo "There is no error :-)."
 fi
