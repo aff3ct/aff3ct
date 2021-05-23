6b81dbb25a2eec8c749a26416637c79fab3c1c66
Adrien Cassagne <adrien.cassagne@inria.fr>
2021-05-23 21:25:02 +0200

    CI: dockerize Windows jobs.

--- ci/build-windows-gcc.bat
+++ ci/build-windows-gcc.bat
@@ -1,13 +1,15 @@
 @echo on
 
+call ./ci/tools/threads.bat
+
 cmake --version
 mkdir build
 cd build
-cmake .. -G"MinGW Makefiles" -DCMAKE_CXX_COMPILER=g++.exe -DCMAKE_BUILD_TYPE=Release %CMAKE_OPT% -DCMAKE_CXX_FLAGS="%CFLAGS%" -DCMAKE_EXE_LINKER_FLAGS="%LFLAGS%" -DCMAKE_INSTALL_PREFIX="%NAME%"
+cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=g++.exe -DCMAKE_BUILD_TYPE=Release %CMAKE_OPT% -DCMAKE_CXX_FLAGS="%CFLAGS%" -DCMAKE_EXE_LINKER_FLAGS="%LFLAGS%" -DCMAKE_INSTALL_PREFIX="%NAME%"
 if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
-mingw32-make -j %THREADS%
+make -j %THREADS%
 if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
 rd /s /q %NAME%
-mingw32-make install > nul
+make install > nul
 if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
 move %NAME% ..\
--- ci/build-windows-msvc.bat
+++ ci/build-windows-msvc.bat
@@ -1,5 +1,7 @@
 @echo on
 
+call ./ci/tools/threads.bat
+
 set "VSCMD_START_DIR=%CD%"
 call "%VS_PATH%\VC\Auxiliary\Build\vcvars64.bat"
 
@@ -8,10 +10,14 @@ mkdir build
 cd build
 cmake .. -G"Visual Studio 15 2017 Win64" %CMAKE_OPT% -DCMAKE_CXX_FLAGS="%CFLAGS% /MP%THREADS%" -DCMAKE_INSTALL_PREFIX="%NAME%"
 if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
-devenv /build Release aff3ct.sln
-rem msbuild aff3ct.sln /t:Build /p:Configuration=Release
+rem devenv /build Release aff3ct.sln
+msbuild aff3ct.sln /t:Build /p:Configuration=Release
 if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
-rd /s /q %NAME%
-devenv /build Release aff3ct.sln /project INSTALL > nul
-if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
-move %NAME% ..\
+cd ..
+move build %NAME%
+
+rem Comment : impossible to run the INSTALL target with MSBuild
+rem rd /s /q %NAME%
+rem devenv /build Release aff3ct.sln /project INSTALL > nul
+rem if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
+rem move %NAME% ..\
