3b9c5f42e0e2ec6a312d911f733b39e83e6e87f3
AsriFox <asrifox@yandex.ru>
2023-11-09 13:12:51 +0300

    Updated Puncturer_turbo_DB.cpp: - Added code rates 6/7 and 7/8 from EN 301 545-2; - Changed code rate selection from if/else to switch.

--- src/Module/Puncturer/Turbo_DB/Puncturer_turbo_DB.cpp
+++ src/Module/Puncturer/Turbo_DB/Puncturer_turbo_DB.cpp
@@ -19,19 +19,40 @@ Puncturer_turbo_DB<B,Q>
 
 	float rate = (float)K / (float)N;
 
-	     if (std::round(100.0 * rate) == std::round(100 * 2.0 / 5.0)) coderate = "2/5";
-	else if (std::round(100.0 * rate) == std::round(100 * 1.0 / 3.0)) coderate = "1/3";
-	else if (std::round(100.0 * rate) == std::round(100 * 1.0 / 2.0)) coderate = "1/2";
-	else if (std::round(100.0 * rate) == std::round(100 * 2.0 / 3.0)) coderate = "2/3";
-	else if (std::round(100.0 * rate) == std::round(100 * 3.0 / 4.0)) coderate = "3/4";
-	else if (std::round(100.0 * rate) == std::round(100 * 4.0 / 5.0)) coderate = "4/5";
-	else if (std::round(100.0 * rate) == std::round(100 * 5.0 / 6.0)) coderate = "5/6";
-	else
+	switch (int(rate * 100)) 
 	{
-		std::stringstream message;
-		message << "Unsupported code rate 'R' = 'K' / 'N' ('R' = " << rate << ", 'K' = " << K << ", 'N' = "
-		        << N << "), supported code rates are 2/5, 1/2, 2/3, 3/4, 4/5 and 5/6.";
-		throw tools::invalid_argument(__FILE__, __LINE__, __func__, message.str());
+		case 100 * 1 / 3:
+			coderate = "1/3";
+			break;
+		case 100 * 2 / 5:
+			coderate = "2/5";
+			break;
+		case 100 * 1 / 2:
+			coderate = "1/2";
+			break;
+		case 100 * 2 / 3:
+			coderate = "2/3";
+			break;
+		case 100 * 3 / 4:
+			coderate = "3/4";
+			break;
+		case 100 * 4 / 5:
+			coderate = "4/5";
+			break;
+		case 100 * 5 / 6:
+			coderate = "5/6";
+			break;
+		case 100 * 6 / 7:
+			coderate = "6/7";
+			break;
+		case 100 * 7 / 8:
+			coderate = "7/8";
+			break;
+		default:
+			std::stringstream message;
+			message << "Unsupported code rate 'R' = 'K' / 'N' ('R' = " << rate << ", 'K' = " << K << ", 'N' = "
+					<< N << "), supported code rates are 1/3, 2/5, 1/2, 2/3, 3/4, 4/5, 5/6, 6/7 and 7/8.";
+			throw tools::invalid_argument(__FILE__, __LINE__, __func__, message.str());
 	}
 }
 
@@ -48,39 +69,39 @@ template <typename B, typename Q>
 void Puncturer_turbo_DB<B,Q>
 ::_puncture(const B *X_N1, B *X_N2, const size_t frame_id) const
 {
-	if (coderate == "1/2")// all the AB, all the Y1Y2
+	if (coderate == "1/3")	// Pattern: [1] [1]
+	{
+		std::copy(X_N1, X_N1 + this->N_cw, X_N2);
+	}
+	else if (coderate == "2/5") // Pattern: [11] [10]
 	{
 		std::copy(X_N1, X_N1 + 2 * this->K, X_N2);
-	}
-	else if (coderate == "1/3")// all the AB, all the Y1Y2, all the W1W2
-	{
-		std::copy(X_N1, X_N1 + 3 * this->K, X_N2);
-	}
-	else if (coderate == "2/5")// all the AB, all the Y1Y2 , one W1W2 over two
-	{
-		std::copy(X_N1, X_N1 + 2 * this->K, X_N2);
-		auto j =  2 * this->K;
-		for(auto i = 0; i < this->K; i+=4)
+		auto j = 2 * this->K;
+		for(auto i = 0; i < this->K; i += 4)
 		{
 			X_N2[j++] = X_N1[2 * this->K + i    ];
 			X_N2[j++] = X_N1[2 * this->K + i + 1];
 		}
 	}
-	else if (coderate == "2/3") // all the AB, one Y1Y2 over two
+	else if (coderate == "1/2")	// Pattern: [1] [0]
+	{
+		std::copy(X_N1, X_N1 + 2 * this->K, X_N2);
+	}
+	else if (coderate == "2/3")	// Pattern: [10] [0]
 	{
 		std::copy(X_N1, X_N1 + this->K, X_N2);
-		auto j =  this->K;
-		for(auto i = 0; i < this->K; i+=4)
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 4)
 		{
 			X_N2[j++] = X_N1[this->K + i    ];
 			X_N2[j++] = X_N1[this->K + i + 1];
 		}
 	}
-	else if (coderate == "3/4") // all the AB, two Y1Y2 over six : [101000]
+	else if (coderate == "3/4")	// Pattern: [101000] [0]
 	{
 		std::copy(X_N1, X_N1 + this->K, X_N2);
-		auto j =  this->K;
-		for(auto i = 0; i < this->K; i+=12)
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 12)
 		{
 			X_N2[j++] = X_N1[this->K + i    ];
 			X_N2[j++] = X_N1[this->K + i + 1];
@@ -88,30 +109,58 @@ void Puncturer_turbo_DB<B,Q>
 			X_N2[j++] = X_N1[this->K + i + 5];
 		}
 	}
-	else if (coderate == "4/5") // all the AB, one Y1Y2 over four
+	else if (coderate == "4/5")	// Pattern: [1000] [0]
 	{
 		std::copy(X_N1, X_N1 + this->K, X_N2);
-		auto j =  this->K;
-		for(auto i = 0; i < this->K; i+=8)
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 8)
 		{
 			X_N2[j++] = X_N1[this->K + i    ];
 			X_N2[j++] = X_N1[this->K + i + 1];
 		}
 	}
-	else if (coderate == "5/6") // all the AB, four Y1Y2 over twenty : [10001000100010000000]
+	else if (coderate == "5/6") // Pattern: [10001000100010000000] [0]
 	{
 		std::copy(X_N1, X_N1 + this->K, X_N2);
-		auto j =  this->K;
-		for(auto i = 0; i < this->K; i+=40)
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 40)
+		{
+			X_N2[j++] = X_N1[this->K + i     ];
+			X_N2[j++] = X_N1[this->K + i + 1 ];
+			X_N2[j++] = X_N1[this->K + i + 8 ];
+			X_N2[j++] = X_N1[this->K + i + 9 ];
+			X_N2[j++] = X_N1[this->K + i + 16];
+			X_N2[j++] = X_N1[this->K + i + 17];
+			X_N2[j++] = X_N1[this->K + i + 24];
+			X_N2[j++] = X_N1[this->K + i + 25];
+		}
+	}
+	else if (coderate == "6/7") // Pattern: [100010000000] [0]
+	{
+		std::copy(X_N1, X_N1 + this->K, X_N2);
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 24)
 		{
 			X_N2[j++] = X_N1[this->K + i    ];
 			X_N2[j++] = X_N1[this->K + i + 1];
 			X_N2[j++] = X_N1[this->K + i + 8];
 			X_N2[j++] = X_N1[this->K + i + 9];
-			X_N2[j++] = X_N1[this->K + i + 16];
-			X_N2[j++] = X_N1[this->K + i + 17];
+		}
+	}
+	else if (coderate == "7/8") // Pattern: [1000100000001000000010000000] [0]
+	{
+		std::copy(X_N1, X_N1 + this->K, X_N2);
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 56)
+		{
+			X_N2[j++] = X_N1[this->K + i     ];
+			X_N2[j++] = X_N1[this->K + i + 1 ];
+			X_N2[j++] = X_N1[this->K + i + 8 ];
+			X_N2[j++] = X_N1[this->K + i + 9 ];
 			X_N2[j++] = X_N1[this->K + i + 24];
 			X_N2[j++] = X_N1[this->K + i + 25];
+			X_N2[j++] = X_N1[this->K + i + 40];
+			X_N2[j++] = X_N1[this->K + i + 41];
 		}
 	}
 }
@@ -120,43 +169,43 @@ template <typename B, typename Q>
 void Puncturer_turbo_DB<B,Q>
 ::_depuncture(const Q *Y_N1, Q *Y_N2, const size_t frame_id) const
 {
-	if (coderate == "1/2") // all the AB, all the Y1Y2
+	if (coderate == "1/3")	// Pattern: [1] [1]
 	{
-		std::copy(Y_N1              , Y_N1 + 2 * this->K, Y_N2);
-		std::fill(Y_N2 + 2 * this->K, Y_N2 + 3 * this->K, (Q)0);
+		std::copy(Y_N1, Y_N1 + this->N_cw, Y_N2);
 	}
-	else if (coderate == "1/3") // all the AB, all the Y1Y2, all the W1W2
+	else if (coderate == "2/5") // Pattern: [11] [10]
 	{
-		std::copy(Y_N1              , Y_N1 + 3 * this->K, Y_N2);
-	}
-	else if (coderate == "2/5")// all the AB, all the Y1Y2 , one W1W2 over two
-	{
-		std::copy(Y_N1              , Y_N1 + 2 * this->K, Y_N2);
-		std::fill(Y_N2 + 2 * this->K, Y_N2 + 3 * this->K, (Q)0);
-		auto j =  2 * this->K;
-		for(auto i = 0; i < this->K; i+=4)
+		std::copy(Y_N1, Y_N1 + 2 * this->K, Y_N2);
+		std::fill(Y_N2 + 2 * this->K, Y_N2 + this->N_cw, (Q)0);
+		auto j = 2 * this->K;
+		for(auto i = 0; i < this->K; i += 4)
 		{
 			Y_N2[2 * this->K + i    ] = Y_N1[j++];
 			Y_N2[2 * this->K + i + 1] = Y_N1[j++];
 		}
 	}
-	else if (coderate == "2/3") // all the AB, one Y1Y2 over two
+	else if (coderate == "1/2")	// Pattern: [1] [0]
 	{
-		std::copy(Y_N1          , Y_N1 +     this->K, Y_N2);
-		std::fill(Y_N2 + this->K, Y_N2 + 3 * this->K, (Q)0);
-		auto j =  this->K;
-		for(auto i = 0; i < this->K; i+=4)
+		std::copy(Y_N1, Y_N1 + 2 * this->K, Y_N2);
+		std::fill(Y_N2 + 2 * this->K, Y_N2 + this->N_cw, (Q)0);
+	}
+	else if (coderate == "2/3")	// Pattern: [10] [0]
+	{
+		std::copy(Y_N1, Y_N1 + this->K, Y_N2);
+		std::fill(Y_N2 + this->K, Y_N2 + this->N_cw, (Q)0);
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 4)
 		{
 			Y_N2[this->K + i    ] = Y_N1[j++];
 			Y_N2[this->K + i + 1] = Y_N1[j++];
 		}
 	}
-	else if (coderate == "3/4") // all the AB, two Y1Y2 over six : [101000]
+	else if (coderate == "3/4")	// Pattern: [101000] [0]
 	{
-		std::copy(Y_N1          , Y_N1 +     this->K, Y_N2);
-		std::fill(Y_N2 + this->K, Y_N2 + 3 * this->K, (Q)0);
-		auto j =  this->K;
-		for(auto i = 0; i < this->K; i+=12)
+		std::copy(Y_N1, Y_N1 + this->K, Y_N2);
+		std::fill(Y_N2 + this->K, Y_N2 + this->N_cw, (Q)0);
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 12)
 		{
 			Y_N2[this->K + i    ] = Y_N1[j++];
 			Y_N2[this->K + i + 1] = Y_N1[j++];
@@ -164,32 +213,62 @@ void Puncturer_turbo_DB<B,Q>
 			Y_N2[this->K + i + 5] = Y_N1[j++];
 		}
 	}
-	else if (coderate == "4/5") // all the AB, one Y1Y2 over four
+	else if (coderate == "4/5")	// Pattern: [1000] [0]
 	{
-		std::copy(Y_N1          , Y_N1 +     this->K, Y_N2);
-		std::fill(Y_N2 + this->K, Y_N2 + 3 * this->K, (Q)0);
-		auto j =  this->K;
-		for(auto i = 0; i < this->K; i+=8)
+		std::copy(Y_N1, Y_N1 + this->K, Y_N2);
+		std::fill(Y_N2 + this->K, Y_N2 + this->N_cw, (Q)0);
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 8)
 		{
 			Y_N2[this->K + i    ] = Y_N1[j++];
 			Y_N2[this->K + i + 1] = Y_N1[j++];
 		}
 	}
-	else if (coderate == "5/6") // all the AB, four Y1Y2 over twenty : [10001000100010000000]
+	else if (coderate == "5/6") // Pattern: [10001000100010000000] [0]
 	{
-		std::copy(Y_N1          , Y_N1 +     this->K, Y_N2);
-		std::fill(Y_N2 + this->K, Y_N2 + 3 * this->K, (Q)0);
-		auto j =  this->K;
-		for(auto i = 0; i < this->K; i+=40)
+		std::copy(Y_N1, Y_N1 + this->K, Y_N2);
+		std::fill(Y_N2 + this->K, Y_N2 + this->N_cw, (Q)0);
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 40)
+		{
+			Y_N2[this->K + i     ] = Y_N1[j++];
+			Y_N2[this->K + i + 1 ] = Y_N1[j++];
+			Y_N2[this->K + i + 8 ] = Y_N1[j++];
+			Y_N2[this->K + i + 9 ] = Y_N1[j++];
+			Y_N2[this->K + i + 16] = Y_N1[j++];
+			Y_N2[this->K + i + 17] = Y_N1[j++];
+			Y_N2[this->K + i + 24] = Y_N1[j++];
+			Y_N2[this->K + i + 25] = Y_N1[j++];
+		}
+	}
+	else if (coderate == "6/7") // Pattern: [100010000000] [0]
+	{
+		std::copy(Y_N1, Y_N1 + this->K, Y_N2);
+		std::fill(Y_N2 + this->K, Y_N2 + this->N_cw, (Q)0);
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 24)
 		{
 			Y_N2[this->K + i    ] = Y_N1[j++];
 			Y_N2[this->K + i + 1] = Y_N1[j++];
 			Y_N2[this->K + i + 8] = Y_N1[j++];
 			Y_N2[this->K + i + 9] = Y_N1[j++];
-			Y_N2[this->K + i + 16] = Y_N1[j++];
-			Y_N2[this->K + i + 17] = Y_N1[j++];
+		}
+	}
+	else if (coderate == "7/8") // Pattern: [1000100000001000000010000000] [0]
+	{
+		std::copy(Y_N1, Y_N1 + this->K, Y_N2);
+		std::fill(Y_N2 + this->K, Y_N2 + this->N_cw, (Q)0);
+		auto j = this->K;
+		for(auto i = 0; i < this->K; i += 56)
+		{
+			Y_N2[this->K + i     ] = Y_N1[j++];
+			Y_N2[this->K + i + 1 ] = Y_N1[j++];
+			Y_N2[this->K + i + 8 ] = Y_N1[j++];
+			Y_N2[this->K + i + 9 ] = Y_N1[j++];
 			Y_N2[this->K + i + 24] = Y_N1[j++];
 			Y_N2[this->K + i + 25] = Y_N1[j++];
+			Y_N2[this->K + i + 40] = Y_N1[j++];
+			Y_N2[this->K + i + 41] = Y_N1[j++];
 		}
 	}
 }
